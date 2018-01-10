b9cdcb09e5f25eb5705e82afff224cb28ed61266
Vasily Beglyanin <vbeglyanin@surfstudio.co>
2018-01-10 21:03:27 +0300

    ANDDEP-7 Перевести Template на Котлин. Работает.

--- template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainActivityView.kt
+++ template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainActivityView.kt
@@ -5,25 +5,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.annotation.IdRes
-
-import javax.inject.Inject
-
 import ru.surfstudio.android.core.ui.base.screen.activity.BaseRenderableHandleableErrorActivityView
 import ru.surfstudio.android.core.ui.base.screen.configurator.BaseActivityConfigurator
 import ru.surfstudio.android.core.ui.base.screen.configurator.ScreenConfigurator
 import ru.surfstudio.android.core.ui.base.screen.presenter.CorePresenter
 import ru.surfstudio.standard.R
 import ru.surfstudio.standard.ui.base.configurator.ActivityConfigurator
+import javax.inject.Inject
 
 /**
  * Вью главного экрана
  */
 class MainActivityView : BaseRenderableHandleableErrorActivityView<MainScreenModel>() {
-    @set:Inject
-    private var presenter: MainPresenter? = null
+    @Inject internal lateinit var presenter: MainPresenter
 
     override fun getPresenters(): Array<CorePresenter<*>> {
-        return arrayOf(presenter as CorePresenter<*>)
+        return arrayOf(presenter)
     }
 
     @IdRes
@@ -40,7 +37,7 @@ class MainActivityView : BaseRenderableHandleableErrorActivityView<MainScreenMod
     }
 
 
-    override fun onCreate(savedInstanceState: Bundle,
+    override fun onCreate(savedInstanceState: Bundle?,
                           persistentState: PersistableBundle?,
                           viewRecreated: Boolean) {
         super.onCreate(savedInstanceState, persistentState, viewRecreated)
