d508984fc0f5c98e651a4dccc873ca7a934f2a03
Vasily Beglyanin <vbeglyanin@surfstudio.co>
2018-01-17 19:06:38 +0300

    ANDDEP-42 удалить содержимое :template ...interactor.common

--- template/src/main/java/ru/surfstudio/standard/interactor/common/error/NotModifiedException.kt
+++
@@ -1,8 +0,0 @@
-package ru.surfstudio.standard.interactor.common.error
-
-import retrofit2.adapter.rxjava.HttpException
-
-/**
- * ошибка 304, см механизм Etag
- */
-class NotModifiedException(cause: HttpException, message: String, code: Int, url: String) : HttpProtocolException(cause, message, code, url)
