df21de2d84f39d6718e6a46baac79f970e099971
vsokolova <vsokolova@surfstudio.co>
2018-02-02 15:02:26 +0300

    #ANDDEP-67 создала extension функцию для View, запускающую лямбду, если только чтото в данных изменилось

---
+++ core/src/main/java/ru/surfstudio/android/core/ui/util/ViewRenderUtils.kt
@@ -0,0 +1,14 @@
+package ru.surfstudio.android.core.ui.util
+
+import android.view.View
+
+/**
+ * Extention метод, который запускает action если данные изменились
+ */
+fun <T> View.actionIfChanged(data: T, action: (data: T) -> Unit) {
+    val hash = data?.hashCode()
+    if (this.tag != hash) {
+        action.invoke(data)
+    }
+    this.tag = hash
+}
