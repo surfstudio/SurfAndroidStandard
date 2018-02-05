697020c072ca6e228d1fdbd49354d1db5571b416
Artem Zaytsev <azaytsev@surfstudio.ru>
2018-02-05 09:49:33 +0300

    ANDDEP-29 Title + Subtitle - лишнее

--- template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainActivityView.kt
+++ template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainActivityView.kt
@@ -3,12 +3,8 @@ package ru.surfstudio.standard.ui.screen.main
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.annotation.IdRes
-import com.jakewharton.rxbinding2.widget.RxTextView
-import org.jetbrains.anko.find
-import org.jetbrains.anko.toast
 import ru.surfstudio.android.core.ui.base.screen.activity.BaseRenderableHandleableErrorActivityView
 import ru.surfstudio.android.core.ui.base.screen.presenter.CorePresenter
-import ru.surfstudio.android.core.ui.base.screen.widjet.TitleSubtitleView
 import ru.surfstudio.standard.R
 import ru.surfstudio.standard.ui.base.configurator.ActivityScreenConfigurator
 import javax.inject.Inject
@@ -18,9 +14,8 @@ import javax.inject.Inject
  */
 class MainActivityView : BaseRenderableHandleableErrorActivityView<MainScreenModel>() {
 
-    @Inject internal lateinit var presenter: MainPresenter
-
-    val titleView by lazy { findViewById<TitleSubtitleView>(R.id.title_subtitle_view) }
+    @Inject
+    internal lateinit var presenter: MainPresenter
 
     override fun getPresenters(): Array<CorePresenter<*>> {
         return arrayOf(presenter)
@@ -42,9 +37,6 @@ class MainActivityView : BaseRenderableHandleableErrorActivityView<MainScreenMod
     }
 
     override fun renderInternal(screenModel: MainScreenModel) {
-        titleView.onTitleClickListenerCallback = { toast(it) }
-        titleView.onSubTitleClickListenerCallback = { toast(it)}
 
-        RxTextView.textChanges(find(R.id.et)).subscribe { titleView.titleText = it.toString() }
     }
 }
