aad5fc9df8b3a8953955c7087c3ee8d47c448b17
vsokolova <vsokolova@surfstudio.co>
2018-02-15 15:16:22 +0300

    #ANDDEP-81 добавила дополнительные методы

--- core/src/main/java/ru/surfstudio/android/core/util/StringsProvider.kt
+++ core/src/main/java/ru/surfstudio/android/core/util/StringsProvider.kt
@@ -1,6 +1,7 @@
 package ru.surfstudio.android.core.util
 
 import android.content.Context
+import android.support.annotation.PluralsRes
 import android.support.annotation.StringRes
 import javax.inject.Inject
 
@@ -9,7 +10,19 @@ import javax.inject.Inject
  */
 class StringsProvider @Inject constructor(var context: Context) {
 
-    fun getString(@StringRes id: Int): String {
-        return context.resources.getString(id)
+    fun getString(@StringRes id: Int, vararg args: Any): String {
+        return if (args.isEmpty()) {
+            context.resources.getString(id)
+        } else {
+            context.resources.getString(id, *args)
+        }
+    }
+
+    fun getQuantityString(@PluralsRes id: Int, quantity: Int, vararg args: Any): String {
+        return if (args.isEmpty()) {
+            context.resources.getQuantityString(id, quantity)
+        } else {
+            context.resources.getQuantityString(id, quantity, *args)
+        }
     }
 }
