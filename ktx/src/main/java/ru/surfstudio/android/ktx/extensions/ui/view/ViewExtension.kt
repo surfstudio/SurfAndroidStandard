332eb028c0aec0c57a6f068656e1af98214f3d05
Artem Zaytsev <azaytsev@surfstudio.ru>
2018-02-15 12:14:18 +0300

    ANDDEP-75 Сделать модуль с полезными extension-функциями - обобщенный actionIfNeeded

--- ktx/src/main/java/ru/surfstudio/android/ktx/extensions/ui/view/ViewExtension.kt
+++ ktx/src/main/java/ru/surfstudio/android/ktx/extensions/ui/view/ViewExtension.kt
@@ -46,10 +46,27 @@ fun View.goneIf(gone: Boolean) {
 /**
  * Extension метод, который запускает action если данные изменились
  */
-fun <T : View, R> T.actionIfChanged(data: R, action: T.(data: R) -> Unit) {
-    val hash = data?.hashCode()
+fun <T : View, R> T.actionIfChanged(data: R?, action: T.(data: R?) -> Unit) {
+    this.actionIfChanged(data, null,null,null,  {d,_,_,_ -> action(d)})
+}
+
+fun <T : View, R1,R2> T.actionIfChanged(data1: R1?,data2: R2?, action: T.(R1?,R2?) -> Unit) {
+    this.actionIfChanged(data1, data2,null,null,  {d1,d2,_,_ -> action(d1,d2)})
+}
+
+fun <T : View, R1,R2,R3> T.actionIfChanged(data1: R1?,data2: R2?, data3: R3? = null, action: T.(R1?,R2?,R3?) -> Unit) {
+    this.actionIfChanged(data1, data2,data3,null,  {d1,d2,d3,_ -> action(d1,d2,d3)})
+}
+
+fun <T : View, R1, R2, R3, R4> T.actionIfChanged(
+        data1: R1, data2: R2? = null, data3: R3? = null, data4: R4? = null,
+        action: T.(data1: R1?, data2: R2?, data3: R3?, data4: R4?) -> Unit) {
+    val hash = data1?.hashCode()
+            ?.plus(data2?.hashCode() ?: 0)
+            ?.plus(data3?.hashCode() ?: 0)
+            ?.plus(data4?.hashCode() ?: 0)
     if (this.tag != hash) {
-        action(data)
+        action(data1,data2, data3, data4)
         this.tag = hash
     }
 }
