7339df052919ce516bae8518727287bbc7250c7e
vsokolova <vsokolova@surfstudio.co>
2018-02-16 12:47:49 +0300

    #ANDDEP-81 правки к ПР

--- template/src/main/java/ru/surfstudio/standard/app/dagger/AppComponent.kt
+++ template/src/main/java/ru/surfstudio/standard/app/dagger/AppComponent.kt
@@ -3,8 +3,8 @@ package ru.surfstudio.standard.app.dagger
 import android.content.Context
 import dagger.Component
 import ru.surfstudio.android.connection.ConnectionProvider
-import ru.surfstudio.android.core.app.scheduler.SchedulersProvider
 import ru.surfstudio.android.core.app.ActiveActivityHolder
+import ru.surfstudio.android.core.app.scheduler.SchedulersProvider
 import ru.surfstudio.android.core.util.StringsProvider
 import ru.surfstudio.android.dagger.scope.PerApplication
 import ru.surfstudio.standard.app.intialization.InitializeAppInteractor
@@ -20,10 +20,10 @@ import ru.surfstudio.standard.interactor.common.network.OkHttpModule
 @Component(modules = [
     (AppModule::class),
     (MigrationModule::class),
-(ActiveActivityHolderModule::class),
-(AuthModule::class),
-(NetworkModule::class),
-(OkHttpModule::class),
+    (ActiveActivityHolderModule::class),
+    (AuthModule::class),
+    (NetworkModule::class),
+    (OkHttpModule::class),
     (AnalyticsModule::class)])
 interface AppComponent {
     fun initializeAppInteractor(): InitializeAppInteractor
