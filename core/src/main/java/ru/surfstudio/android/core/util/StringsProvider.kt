58602f94a42e94e5aedd2e67f39477c8a9f1fc76
vsokolova <vsokolova@surfstudio.co>
2018-02-16 12:50:11 +0300

    #ANDDEP-81 правки к ПР

--- core/src/main/java/ru/surfstudio/android/core/util/StringsProvider.kt
+++ core/src/main/java/ru/surfstudio/android/core/util/StringsProvider.kt
@@ -3,11 +3,13 @@ package ru.surfstudio.android.core.util
 import android.content.Context
 import android.support.annotation.PluralsRes
 import android.support.annotation.StringRes
+import ru.surfstudio.android.dagger.scope.PerApplication
 import javax.inject.Inject
 
 /**
  * Класс, предоставляющий строки из ресурсов по id
  */
+@PerApplication
 class StringsProvider @Inject constructor(var context: Context) {
 
     fun getString(@StringRes id: Int, vararg args: Any): String {
