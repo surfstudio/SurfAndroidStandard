c30fa2d8bdaef9b5af9788b1162f4c11923e41ad
Artem Zaytsev <azaytsev@surfstudio.ru>
2018-02-21 13:38:57 +0300

    ANDDEP-90 Правки и рефакторинг модуля с Пушами - убран даггер

--- push/src/main/java/ru/surfstudio/android/notification/NotificationCenter.kt
+++ push/src/main/java/ru/surfstudio/android/notification/NotificationCenter.kt
@@ -2,30 +2,35 @@ package ru.surfstudio.android.notification
 
 import android.content.Context
 import ru.surfstudio.android.core.app.ActiveActivityHolder
+import ru.surfstudio.android.notification.interactor.push.PushInteractor
 import ru.surfstudio.android.notification.ui.notification.AbstractPushHandleStrategyFactory
+import ru.surfstudio.android.notification.ui.notification.PushHandler
 
 /**
  * Помощник инициализации нотификаций
  */
 object NotificationCenter {
 
-    lateinit var activeActivityHolder: ActiveActivityHolder
-    lateinit var pushHandleStrategyFactory: AbstractPushHandleStrategyFactory
+    private lateinit var activeActivityHolder: ActiveActivityHolder
+    private lateinit var pushHandleStrategyFactory: AbstractPushHandleStrategyFactory
 
-    fun configure( conf: NotificationCenter.() -> Unit) {
-        this.apply(conf)
-    }
+    private lateinit var pushHandler: PushHandler
 
-    val notificationComponent: NotificationComponent by lazy {
-        DaggerNotificationComponent.builder()
-                .notificationModule(NotificationModule(activeActivityHolder, pushHandleStrategyFactory))
-                .build()
+    fun setActiveActivityHolder(activeActivityHolder: ActiveActivityHolder) =
+            apply { this.activeActivityHolder = activeActivityHolder }
+
+    fun setPushHandleStrategyFactory(pushHandleStrategyFactory: AbstractPushHandleStrategyFactory) =
+            apply { this.pushHandleStrategyFactory = pushHandleStrategyFactory }
+
+    fun configure(conf: NotificationCenter.() -> Unit) {
+        this.conf()
+        pushHandler = PushHandler(activeActivityHolder, pushHandleStrategyFactory, PushInteractor())
     }
 
     /**
      * Обработка сообщения из FirebaseMessagingService
      */
     fun onReceiveMessage(context: Context, title: String, body: String, data: Map<String, String>) {
-        notificationComponent.pushHandler().handleMessage(context, title, body, data)
+        pushHandler.handleMessage(context, title, body, data)
     }
 }
