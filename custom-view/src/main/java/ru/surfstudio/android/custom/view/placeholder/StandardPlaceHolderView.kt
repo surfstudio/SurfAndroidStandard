dc6ddfec4a4af4b08f919596da5be8d4b50297f0
Saturov <saturov@yandex.ru>
2018-02-22 21:37:54 +0300

    ANDDEP-50 Исправить PlaceHolderViewImp в template - Выработка системы стилей

--- custom-view/src/main/java/ru/surfstudio/android/custom/view/placeholder/StandardPlaceHolderView.kt
+++ custom-view/src/main/java/ru/surfstudio/android/custom/view/placeholder/StandardPlaceHolderView.kt
@@ -1,12 +1,14 @@
 package ru.surfstudio.android.custom.view.placeholder
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import ru.surfstudio.android.core.mvp.model.state.LoadState
 import ru.surfstudio.android.core.mvp.placeholder.PlaceHolderView
 import ru.surfstudio.android.custom.view.R
+import ru.surfstudio.android.logger.Logger
 
 class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
                                                         attrs: AttributeSet,
@@ -14,13 +16,23 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
     : FrameLayout(context, attrs, defStyle),
         PlaceHolderView {
 
+    private var opaqueBackgroundColor = Color.TRANSPARENT
+    private var transparentBackgroundColor = Color.TRANSPARENT
+
     init {
         View.inflate(context, R.layout.placeholder_view_layout, this)
 
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.PlaceHolderView, defStyle, R.style.Widget_PlaceHolder)
+        applyAttributes(context, attrs, defStyle)
     }
 
     override fun render(loadState: LoadState?) {
 
+    }
+
+    private fun applyAttributes(context: Context, attrs: AttributeSet, defStyle: Int) {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.PlaceHolderView, defStyle, R.style.PlaceHolderView_Default)
+        this.opaqueBackgroundColor = ta.getColor(R.styleable.PlaceHolderView_opaqueBackgroundColor, Color.TRANSPARENT)
+        this.transparentBackgroundColor = ta.getColor(R.styleable.PlaceHolderView_transparentBackgroundColor, Color.TRANSPARENT)
+        ta.recycle()
     }
 }
