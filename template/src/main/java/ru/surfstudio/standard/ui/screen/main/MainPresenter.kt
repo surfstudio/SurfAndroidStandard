bd6390a08f7503e64f328fe0f9b58ea4c1fcb049
Vasily Beglyanin <vbeglyanin@surfstudio.co>
2018-02-26 18:54:16 +0300

    hotfix. Проблема при запуске приложения.

--- template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainPresenter.kt
+++ template/src/main/java/ru/surfstudio/standard/ui/screen/main/MainPresenter.kt
@@ -3,7 +3,10 @@ package ru.surfstudio.standard.ui.screen.main
 import ru.surfstudio.android.core.mvp.presenter.BasePresenter
 import ru.surfstudio.android.core.mvp.presenter.BasePresenterDependency
 import ru.surfstudio.android.core.ui.message.MessageController
+import ru.surfstudio.android.core.ui.permission.PermissionManager
 import ru.surfstudio.android.dagger.scope.PerScreen
+import ru.surfstudio.android.picturechooser.CameraStoragePermissionRequest
+import ru.surfstudio.android.picturechooser.PicturePermissionChecker
 import ru.surfstudio.android.picturechooser.PictureProvider
 import ru.surfstudio.standard.interactor.analytics.AnalyticsService
 import ru.surfstudio.standard.interactor.analytics.event.EnterEvent
@@ -13,7 +16,9 @@ import javax.inject.Inject
  * Презентер главного экрана
  */
 @PerScreen
-internal class MainPresenter @Inject constructor(private val analyticsService: AnalyticsService,basePresenterDependency: BasePresenterDependency,
+internal class MainPresenter @Inject constructor(private val analyticsService: AnalyticsService, basePresenterDependency: BasePresenterDependency,
+                                                 private val permissionManager: PermissionManager,
+                                                 private val picturePermissionChecker: PicturePermissionChecker,
                                                  private val photoProvider: PictureProvider,
                                                  private val messageController: MessageController) : BasePresenter<MainActivityView>(basePresenterDependency) {
 
@@ -22,6 +27,7 @@ internal class MainPresenter @Inject constructor(private val analyticsService: A
     override fun onLoad(viewRecreated: Boolean) {
         super.onLoad(viewRecreated)
         view.render(screenModel)
+        subscribeIoHandleError(picturePermissionChecker.checkCameraStoragePermission(), { _ -> })
 
 
         analyticsService.sendEvent(EnterEvent())
@@ -43,5 +49,15 @@ internal class MainPresenter @Inject constructor(private val analyticsService: A
         subscribeIoHandleError(photoProvider.openGalleryAndGetFewPhoto(), { path ->
             messageController.show(path.toString())
         })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (permissionManager.check(CameraStoragePermissionRequest())) view.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        view.stopCamera()
     }
 }
