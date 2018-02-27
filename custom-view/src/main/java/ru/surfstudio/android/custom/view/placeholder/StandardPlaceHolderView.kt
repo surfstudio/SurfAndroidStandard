22ac7de60d43dc87ee0d1083e047e247c0b069f3
Eugene Saturov <saturov@surfstudio.ru>
2018-02-27 12:05:12 +0300

    ANDDEP-50 Исправить PlaceHolderViewImp в template - Менеджер данных

--- custom-view/src/main/java/ru/surfstudio/android/custom/view/placeholder/StandardPlaceHolderView.kt
+++ custom-view/src/main/java/ru/surfstudio/android/custom/view/placeholder/StandardPlaceHolderView.kt
@@ -1,17 +1,19 @@
 package ru.surfstudio.android.custom.view.placeholder
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
 import io.reactivex.subjects.PublishSubject
 import me.zhanghai.android.materialprogressbar.MaterialProgressBar
 import ru.surfstudio.android.core.mvp.model.state.LoadState
 import ru.surfstudio.android.core.mvp.placeholder.PlaceHolderView
 import ru.surfstudio.android.custom.view.R
-import ru.surfstudio.android.logger.Logger
 import ru.surfstudio.android.utilktx.ktx.ui.textview.setTextOrGone
 
 const val NOT_ASSIGNED = -1 //заглушка для незаданного атрибута
@@ -25,6 +27,9 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
     private var progressBar: MaterialProgressBar
     private var titleTv: TextView
     private var subtitleTv: TextView
+    private var button: Button
+    private var secondButton: Button
+    private var imageIv: ImageView
 
     private val styler = PlaceholderStyler()                   //менеджер стилизации плейсхолдера
     private val dataContainer = PlaceholderDataContainer()     //менеджер данных плейсхолдера
@@ -35,6 +40,9 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
         progressBar = findViewById(R.id.placeholder_loading_pb)
         titleTv = findViewById(R.id.placeholder_title_tv)
         subtitleTv = findViewById(R.id.placeholder_subtitle_tv)
+        button = findViewById(R.id.placeholder_first_btn)
+        secondButton = findViewById(R.id.placeholder_second_btn)
+        imageIv = findViewById(R.id.placeholder_image_iv)
         applyAttributes(context, attrs, defStyle)
     }
 
@@ -44,12 +52,16 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
 
     private fun applyAttributes(context: Context, attrs: AttributeSet, defStyle: Int) {
         val ta = context.obtainStyledAttributes(attrs, R.styleable.PlaceHolderView, defStyle, R.style.PlaceHolderView)
+
         this.styler.opaqueBackgroundColor = ta.getResourceId(R.styleable.PlaceHolderView_opaqueBackgroundColor, NOT_ASSIGNED)
         this.styler.transparentBackgroundColor = ta.getResourceId(R.styleable.PlaceHolderView_transparentBackgroundColor, NOT_ASSIGNED)
         this.styler.progressBarColor = ta.getResourceId(R.styleable.PlaceHolderView_progressBarColor, NOT_ASSIGNED)
 
-        this.dataContainer.title = ta.getString(R.styleable.PlaceHolderView_title)
-        this.dataContainer.subtitle = ta.getString(R.styleable.PlaceHolderView_subtitle)
+        this.dataContainer.title = ta.getString(R.styleable.PlaceHolderView_title) ?: ""
+        this.dataContainer.subtitle = ta.getString(R.styleable.PlaceHolderView_subtitle) ?: ""
+        this.dataContainer.buttonText = ta.getString(R.styleable.PlaceHolderView_buttonText) ?: ""
+        this.dataContainer.secondButtonText = ta.getString(R.styleable.PlaceHolderView_secondButtonText) ?: ""
+        //this.dataContainer.imageHolder.image = ContextCompat.getDrawable(context, ta.getResourceId(R.styleable.PlaceHolderView_image))
 
         updateView()
 
@@ -89,11 +101,13 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
     }
 
     /**
-     * Инициализация плейсхолдера данными
+     * Инициализация плейсхолдера данными.
      */
     private fun setData() {
         titleTv.setTextOrGone(dataContainer.title)
         subtitleTv.setTextOrGone(dataContainer.subtitle)
+        button.setTextOrGone(dataContainer.buttonText)
+        secondButton.setTextOrGone(dataContainer.secondButtonText)
     }
 
     /**
@@ -121,16 +135,27 @@ class StandardPlaceHolderView @JvmOverloads constructor(context: Context,
      * Хранилище всех данных [StandardPlaceHolderView].
      */
     data class PlaceholderDataContainer(var title: String = "",
-                                        var subtitle: String = "")
+                                        var subtitle: String = "",
+                                        var buttonText: String = "",
+                                        var secondButtonText: String = "",
+                                        var imageHolder: PlaceHolderImageHolder = PlaceHolderImageHolder()) {
+
+        data class PlaceHolderImageHolder(var image: Drawable? = null,
+                                          var emptyImage: Drawable? = null,
+                                          var notFoundImage: Drawable? = null,
+                                          var errorImage: Drawable? = null)
+    }
 
     /**
      * Менеджер текущего состояния [StandardPlaceHolderView] и переключения между состояниями.
+     *
+     * @param onStateChangedLambda лямбда, срабатывающая при изменении состояния [StandardPlaceHolderView].
      */
-    class PlaceholderStater(private var onStateChanged: ((loadState: LoadState) -> (Unit))) {
+    class PlaceholderStater(private var onStateChangedLambda: ((loadState: LoadState) -> (Unit))) {
         var loadState = LoadState.NONE          //текущее состояние плейсхолдера
             set(value) {
                 field = value
-                onStateChanged.invoke(field)
+                onStateChangedLambda.invoke(field)
             }
 
         var loadStateSubject: PublishSubject<LoadState> = PublishSubject.create() //шина изменений loadState
