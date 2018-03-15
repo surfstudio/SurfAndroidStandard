542a0050af2d8598d54ca7f9f4c219ce92514fd7
vsokolova <vsokolova@surfstudio.co>
2018-03-15 09:54:35 +0300

    #ANDDEP-101 правки к ПР

--- standard-dialog/src/main/java/com/example/standarddialog/StandardDialogPresenter.kt
+++ standard-dialog/src/main/java/com/example/standarddialog/StandardDialogPresenter.kt
@@ -4,6 +4,6 @@ package com.example.standarddialog
  * Интерфейс, которому должен удовлетворять любой презентер, использующий стандартный диалог
  */
 interface StandardDialogPresenter {
-    fun positiveBtnAction(dialogTag: String)
-    fun negativeBtnAction(dialogTag: String)
+    fun simpleDialogPositiveBtnAction(dialogTag: String)
+    fun simpleDialogNegativeBtnAction(dialogTag: String)
 }
