fc29457cc38b47e9b39c15bba6d884515e54fdc7
Beglyanin <vbeglyanin@surfstudio.co>
2018-04-02 14:55:31 +0300

    Закрытие клавиатуры даже в том случае если фокусбыл потерян.

--- util-ktx/src/main/java/ru/surfstudio/android/utilktx/util/KeyboardUtil.kt
+++ util-ktx/src/main/java/ru/surfstudio/android/utilktx/util/KeyboardUtil.kt
@@ -2,6 +2,7 @@ package ru.surfstudio.android.utilktx.util
 
 import android.app.Activity
 import android.content.Context
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -29,11 +30,8 @@ object KeyboardUtil {
      * Скрыть экранную клавиатуру
      */
     fun hideSoftKeyboard(activity: Activity) {
-        val view = activity.currentFocus
-        if (view != null) {
-            val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
-        }
+        (activity.currentFocus ?: activity.findViewById<View>(android.R.id.content))
+                ?.let { hideSoftKeyboard(it) }
     }
 
     /**
