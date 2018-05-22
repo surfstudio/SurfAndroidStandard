8934c44306ddd7d2ad058c06c5274b3a7d395d28
Alexey Korpatenkov <akorpatenkov@surfstudio.co>
2018-05-22 12:15:10 +0300

    ANDDEP-79 удалёны пакеты после переноса

--- util-ktx/src/main/java/ru/surfstudio/android/utilktx/ktx/datawrapper/DataWrapperInterface.kt
+++
@@ -1,8 +0,0 @@
-package ru.surfstudio.android.utilktx.ktx.datawrapper
-
-/**
- * Интерфейс для расширяемого класса, имеющий какие-либо состояния
- */
-interface DataWrapperInterface<T> {
-    var data: T
-}
