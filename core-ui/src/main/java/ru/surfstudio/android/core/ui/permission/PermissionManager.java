be35f871373f5d2b5213aa3c2b3e4008d44a94c5
Vita Sokolova <vsokolova@surfstudio.co>
2018-06-07 13:56:09 +0300

    #ANDDEP-198 Добавлены методы в PermissionManager, позволяющие понять показывается ли еще диалог запроса пермишена или он уже запрещен

--- core-ui/src/main/java/ru/surfstudio/android/core/ui/permission/PermissionManager.java
+++ core-ui/src/main/java/ru/surfstudio/android/core/ui/permission/PermissionManager.java
@@ -16,7 +16,9 @@
 package ru.surfstudio.android.core.ui.permission;
 
 
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 
 import java.util.HashMap;
@@ -75,6 +77,19 @@ public abstract class PermissionManager implements RequestPermissionsResultDeleg
     }
 
     /**
+     * Проверка условия, должен ли UI показать пояснение, для чего нужен запрашиваемый Permission.
+     *
+     * @return true/false
+     */
+    public boolean shouldShowRequestPermissionRationale(PermissionRequest permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkRequestPermissionRationale(permission);
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * запрашивает разрешение
      *
      * @param request
@@ -108,5 +123,17 @@ public abstract class PermissionManager implements RequestPermissionsResultDeleg
         } else {
             requestSubjects.get(request.getRequestCode()).onNext(true);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean checkRequestPermissionRationale(PermissionRequest request) {
+        boolean result = false;
+        boolean currentPermissionStatus;
+
+        for (String permission : request.getPermissions()) {
+            currentPermissionStatus = activityProvider.get().shouldShowRequestPermissionRationale(permission);
+            result = result || currentPermissionStatus;
+        }
+        return result;
     }
 }
