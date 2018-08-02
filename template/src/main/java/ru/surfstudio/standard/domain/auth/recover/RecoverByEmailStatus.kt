e558a61068ad2fd6e18166a332db49b611644aae
Маргарита Володина <volodina@surfstudio.co>
2018-08-02 17:13:00 +0300

    ANDDEP-229 Удален старый шаблон проектов

--- template/src/main/java/ru/surfstudio/standard/domain/auth/recover/RecoverByEmailStatus.kt
+++
@@ -1,7 +0,0 @@
-package ru.surfstudio.standard.domain.auth.recover
-
-/**
- * Статус восстановления доступа через email
- */
-//todo Обновить или удалить класс в соответствии с нуждами приложения
-data class RecoverByEmailStatus(val email: String) : RecoverStatus()
