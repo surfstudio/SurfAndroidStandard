1aac501bf0df6875561a10ba91b479bff449eea9
Маргарита Володина <volodina@surfstudio.co>
2018-08-08 13:00:40 +0300

    ANDDEP-230 Инициализация mvp-dialog-sample

---
+++ mvp-dialog-sample/src/main/java/ru/surfstudio/android/mvp/dialog/sample/ui/screen/main/MainPresenter.kt
@@ -0,0 +1,23 @@
+package ru.surfstudio.android.mvp.dialog.sample.ui.screen.main
+
+import ru.surfstudio.android.core.mvp.presenter.BasePresenter
+import ru.surfstudio.android.core.mvp.presenter.BasePresenterDependency
+import ru.surfstudio.android.dagger.scope.PerScreen
+import ru.surfstudio.android.mvp.dialog.navigation.navigator.DialogNavigator
+import javax.inject.Inject
+
+/**
+ * Презентер главного экрана
+ */
+@PerScreen
+internal class MainPresenter @Inject constructor(basePresenterDependency: BasePresenterDependency,
+                                                 private val dialogNavigator: DialogNavigator
+) : BasePresenter<MainActivityView>(basePresenterDependency) {
+
+    private val screenModel: MainScreenModel = MainScreenModel()
+
+    override fun onLoad(viewRecreated: Boolean) {
+        super.onLoad(viewRecreated)
+        view.render(screenModel)
+    }
+}
