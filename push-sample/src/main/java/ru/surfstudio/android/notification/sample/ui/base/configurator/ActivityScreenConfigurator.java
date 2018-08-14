f6134b8ca3778951aeb591a8682258d9f6bc660d
Margarita Volodina <volodina@surfstudio.co>
2018-08-14 14:02:04 +0300

    ANDDEP-230 Начало создания push-sample

---
+++ push-sample/src/main/java/ru/surfstudio/android/notification/sample/ui/base/configurator/ActivityScreenConfigurator.java
@@ -0,0 +1,41 @@
+package ru.surfstudio.android.notification.sample.ui.base.configurator;
+
+import android.content.Intent;
+
+import ru.surfstudio.android.core.mvp.configurator.BaseActivityViewConfigurator;
+import ru.surfstudio.android.notification.sample.app.App;
+import ru.surfstudio.android.notification.sample.app.dagger.AppComponent;
+import ru.surfstudio.android.notification.sample.ui.base.dagger.activity.ActivityComponent;
+import ru.surfstudio.android.notification.sample.ui.base.dagger.activity.ActivityModule;
+import ru.surfstudio.android.notification.sample.ui.base.dagger.activity.DaggerActivityComponent;
+import ru.surfstudio.android.notification.sample.ui.base.dagger.screen.ActivityScreenModule;
+
+/**
+ * Базовый конфигуратор для экрана, основанного на активити
+ */
+
+public abstract class ActivityScreenConfigurator
+        extends BaseActivityViewConfigurator<AppComponent, ActivityComponent, ActivityScreenModule> {
+
+    public ActivityScreenConfigurator(Intent intent) {
+        super(intent);
+    }
+
+    @Override
+    protected ActivityComponent createActivityComponent(AppComponent parentComponent) {
+        return DaggerActivityComponent.builder()
+                .appComponent(parentComponent)
+                .activityModule(new ActivityModule(getPersistentScope()))
+                .build();
+    }
+
+    @Override
+    protected AppComponent getParentComponent() {
+        return ((App) (getTargetActivity()).getApplicationContext()).getAppComponent();
+    }
+
+    @Override
+    protected ActivityScreenModule getActivityScreenModule() {
+        return new ActivityScreenModule(getPersistentScope());
+    }
+}
---
+++ push-sample/src/main/java/ru/surfstudio/android/notification/sample/ui/screen/main/MainActivityView.kt
@@ -0,0 +1,42 @@
+package ru.surfstudio.android.notification.sample.ui.screen.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.annotation.IdRes
+
+import ru.surfstudio.android.core.mvp.activity.BaseRenderableActivityView
+import ru.surfstudio.android.core.mvp.presenter.CorePresenter
+import ru.surfstudio.android.notification.sample.R
+import ru.surfstudio.android.notification.sample.ui.base.configurator.ActivityScreenConfigurator
+import javax.inject.Inject
+
+/**
+ * Вью главного экрана
+ */
+class MainActivityView : BaseRenderableActivityView<MainScreenModel>() {
+    override fun getScreenName(): String  = "MainActivity"
+
+    @Inject
+    internal lateinit var presenter: MainPresenter
+
+    @IdRes
+    override fun getContentView(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?,
+                          persistentState: PersistableBundle?,
+                          viewRecreated: Boolean) {
+        super.onCreate(savedInstanceState, persistentState, viewRecreated)
+    }
+
+    override fun renderInternal(screenModel: MainScreenModel) {}
+
+    override fun getPresenters(): Array<CorePresenter<*>> {
+        return arrayOf(presenter)
+    }
+
+    override fun createConfigurator(): ActivityScreenConfigurator {
+        return MainScreenConfigurator(intent)
+    }
+}
