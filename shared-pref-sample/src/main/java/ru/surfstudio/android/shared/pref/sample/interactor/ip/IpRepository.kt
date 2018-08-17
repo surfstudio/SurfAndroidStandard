45102a04d123aea7f44428e4683d3eacb5597bde
Margarita Volodina <volodina@surfstudio.co>
2018-08-17 18:39:59 +0300

    ANDDEP-230 Начало создания shared-pref-sample

---
+++ shared-pref-sample/src/main/java/ru/surfstudio/android/shared/pref/sample/interactor/ip/IpRepository.kt
@@ -0,0 +1,40 @@
+package ru.surfstudio.android.shared.pref.sample.interactor.ip
+
+import io.reactivex.Observable
+import ru.surfstudio.android.connection.ConnectionProvider
+import ru.surfstudio.android.dagger.scope.PerApplication
+import ru.surfstudio.android.shared.pref.sample.domain.ip.Ip
+import ru.surfstudio.android.shared.pref.sample.interactor.ip.cache.IpStorage
+import ru.surfstudio.android.shared.pref.sample.interactor.ip.network.IpApi
+import ru.surfstudio.android.network.BaseNetworkInteractor
+import ru.surfstudio.android.network.TransformUtil
+import javax.inject.Inject
+
+/**
+ * Репозиторий для получения ip
+ */
+@PerApplication
+class IpRepository @Inject constructor(connectionQualityProvider: ConnectionProvider,
+                                       private val ipApi: IpApi,
+                                       private val ipStorage: IpStorage
+) : BaseNetworkInteractor(connectionQualityProvider) {
+
+    fun getIp(): Observable<Ip> {
+        return ipApi.getIp()
+                .map { ipResponse -> TransformUtil.transform(ipResponse) }
+    }
+
+    fun getIpFromCache(): Ip? = ipStorage.ip
+
+    /**
+     * Кэширование ip
+     */
+    fun saveIp(ip: Ip) {
+        ipStorage.ip = ip
+    }
+
+    /**
+     * Очистка кэша
+     */
+    fun clearIpStorage() = ipStorage.clear()
+}
---
+++ shared-pref-sample/src/main/java/ru/surfstudio/android/shared/pref/sample/interactor/ip/cache/IpStorage.kt
@@ -0,0 +1,41 @@
+package ru.surfstudio.android.shared.pref.sample.interactor.ip.cache
+
+import com.google.gson.GsonBuilder
+import ru.surfstudio.android.dagger.scope.PerApplication
+import ru.surfstudio.android.filestorage.BaseLocalCache
+import ru.surfstudio.android.filestorage.CacheConstant.INTERNAL_CACHE_DIR_DAGGER_NAME
+import ru.surfstudio.android.filestorage.ObjectConverter
+import ru.surfstudio.android.filestorage.naming.NamingProcessor
+import ru.surfstudio.android.filestorage.processor.CacheFileProcessor
+import ru.surfstudio.android.shared.pref.sample.domain.ip.Ip
+import javax.inject.Inject
+import javax.inject.Named
+
+/**
+ * Локальное хранилище информации об ip
+ */
+@PerApplication
+class IpStorage @Inject constructor(@Named(INTERNAL_CACHE_DIR_DAGGER_NAME) cacheDir: String
+) : BaseLocalCache<Ip>(
+        CacheFileProcessor(cacheDir, KEY_IP_STORAGE, 1),
+        NamingProcessor { rawName -> rawName }) {
+
+    companion object {
+        private const val KEY_IP_STORAGE = "ip_storage"
+    }
+
+    private val gsonBuilder = GsonBuilder().create()
+
+    override fun getConverter(): ObjectConverter<Ip> = object : ObjectConverter<Ip> {
+
+        override fun encode(value: Ip): ByteArray = gsonBuilder.toJson(value).toByteArray()
+
+        override fun decode(rawValue: ByteArray): Ip = gsonBuilder.fromJson(String(rawValue), Ip::class.java)
+    }
+
+    var ip: Ip?
+        get() = get(KEY_IP_STORAGE)
+        set(value) {
+            value?.let { put(KEY_IP_STORAGE, it) }
+        }
+}
