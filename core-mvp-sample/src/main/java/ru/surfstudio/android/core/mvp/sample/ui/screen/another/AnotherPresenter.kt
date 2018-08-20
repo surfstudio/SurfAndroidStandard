c62450bc9ed697827d2b5b42c63398b23a7d2bb7
Margarita Volodina <volodina@surfstudio.co>
2018-08-20 13:56:39 +0300

    ANDDEP-230 Завершен core-mvp-sample

--- core-mvp-sample/src/main/java/ru/surfstudio/android/core/mvp/sample/ui/screen/another/AnotherPresenter.kt
+++ core-mvp-sample/src/main/java/ru/surfstudio/android/core/mvp/sample/ui/screen/another/AnotherPresenter.kt
@@ -2,11 +2,13 @@ package ru.surfstudio.android.core.mvp.sample.ui.screen.another
 
 import ru.surfstudio.android.core.mvp.presenter.BasePresenter
 import ru.surfstudio.android.core.mvp.presenter.BasePresenterDependency
+import ru.surfstudio.android.core.mvp.sample.ui.core.CustomOnDestroyDelegate
 import ru.surfstudio.android.dagger.scope.PerScreen
 import javax.inject.Inject
 
 @PerScreen
-internal class AnotherPresenter @Inject constructor(basePresenterDependency: BasePresenterDependency
+internal class AnotherPresenter @Inject constructor(basePresenterDependency: BasePresenterDependency,
+                                                    customOnDestroyDelegate: CustomOnDestroyDelegate
 ) : BasePresenter<AnotherActivityView>(basePresenterDependency) {
 
     private val screenModel = AnotherScreenModel()
