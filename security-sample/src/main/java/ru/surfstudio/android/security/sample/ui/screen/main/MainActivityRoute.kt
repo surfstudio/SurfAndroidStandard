d36751ba61bb51f6a80ff0422654aa6932bc1c29
Margarita Volodina <volodina@surfstudio.co>
2018-09-17 10:29:11 +0300

    ANDDEP-82 Исправление

--- security-sample/src/main/java/ru/surfstudio/android/security/sample/ui/screen/main/MainActivityRoute.kt
+++ security-sample/src/main/java/ru/surfstudio/android/security/sample/ui/screen/main/MainActivityRoute.kt
@@ -2,20 +2,13 @@ package ru.surfstudio.android.security.sample.ui.screen.main
 
 import android.content.Context
 import android.content.Intent
-import ru.surfstudio.android.core.ui.navigation.Route
 import ru.surfstudio.android.core.ui.navigation.activity.route.ActivityRoute
 
 /**
  * Роут главного экрана
- * @param isReAuthState переавторизация после таймаута в бэкграунде
  */
-class MainActivityRoute(private val isReAuthState: Boolean = false) : ActivityRoute() {
-
-    override fun prepareIntent(context: Context) = Intent(context, MainActivityView::class.java)
-            .apply {
-                putExtra(Route.EXTRA_FIRST, isReAuthState)
-                if (isReAuthState) {
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                }
-            }
+class MainActivityRoute : ActivityRoute() {
+    override fun prepareIntent(context: Context): Intent {
+        return Intent(context, MainActivityView::class.java)
+    }
 }
