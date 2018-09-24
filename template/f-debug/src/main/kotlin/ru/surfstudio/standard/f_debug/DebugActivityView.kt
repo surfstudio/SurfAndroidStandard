2115112767f12dbabeb2f58bb66f315376f926ba
Margarita Volodina <volodina@surfstudio.co>
2018-09-24 18:45:22 +0300

    ANDDEP-272 Изменение отображения debug-screen

--- template/f-debug/src/main/kotlin/ru/surfstudio/standard/f_debug/DebugActivityView.kt
+++ template/f-debug/src/main/kotlin/ru/surfstudio/standard/f_debug/DebugActivityView.kt
@@ -3,11 +3,16 @@ package ru.surfstudio.standard.f_debug
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.annotation.LayoutRes
+import android.support.annotation.StringRes
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_debug.*
 import ru.surfstudio.android.core.mvp.activity.BaseRenderableActivityView
 import ru.surfstudio.android.core.mvp.presenter.CorePresenter
+import ru.surfstudio.android.easyadapter.EasyAdapter
+import ru.surfstudio.android.easyadapter.ItemList
 import ru.surfstudio.android.template.f_debug.R
 import ru.surfstudio.standard.base_ui.component.provider.ComponentProvider
+import ru.surfstudio.standard.f_debug.controller.DebugItemController
 import javax.inject.Inject
 
 /**
@@ -17,6 +22,8 @@ class DebugActivityView : BaseRenderableActivityView<DebugScreenModel>() {
 
     @Inject
     lateinit var presenter: DebugPresenter
+
+    private val adapter = EasyAdapter()
 
     override fun getPresenters(): Array<CorePresenter<*>> = arrayOf(presenter)
 
@@ -29,14 +36,35 @@ class DebugActivityView : BaseRenderableActivityView<DebugScreenModel>() {
                           persistentState: PersistableBundle?,
                           viewRecreated: Boolean) {
         super.onCreate(savedInstanceState, persistentState, viewRecreated)
-        initListeners()
+        initRecycler()
+        initAdapter()
     }
 
     override fun renderInternal(screenModel: DebugScreenModel) { }
 
     override fun getScreenName(): String = "debug"
 
-    private fun initListeners() {
-        show_fcm_token_btn.setOnClickListener { presenter.openFcmTokenScreen() }
+    private fun initRecycler() {
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
+    }
+
+    private fun initAdapter() {
+        val showControllersItemController = createDebugItemController(R.string.show_controllers_debug_item) {
+            presenter.openControllersScreen()
+        }
+
+        val showFcmTokenItemController = createDebugItemController(R.string.show_fcm_token_debug_item) {
+            presenter.openFcmTokenScreen()
+        }
+
+        adapter.setItems(ItemList.create()
+                .add(showControllersItemController)
+                .add(showFcmTokenItemController))
+    }
+
+    private fun createDebugItemController(@StringRes nameId: Int,
+                                          onClickListener: () -> Unit): DebugItemController {
+        return DebugItemController(getString(nameId), onClickListener)
     }
 }
