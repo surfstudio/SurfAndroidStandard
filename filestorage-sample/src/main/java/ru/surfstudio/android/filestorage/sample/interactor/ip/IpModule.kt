56045b1d29f4225bda54bc595eaf0d56777b425a
Margarita Volodina <volodina@surfstudio.co>
2018-09-25 18:54:16 +0300

    ANDDEP-273 Удален dagger из модуля network + исправлен filestorage-sample

--- filestorage-sample/src/main/java/ru/surfstudio/android/filestorage/sample/interactor/ip/IpModule.kt
+++ filestorage-sample/src/main/java/ru/surfstudio/android/filestorage/sample/interactor/ip/IpModule.kt
@@ -4,12 +4,20 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import ru.surfstudio.android.dagger.scope.PerApplication
+import ru.surfstudio.android.filestorage.sample.interactor.ip.cache.IpStorage
 import ru.surfstudio.android.filestorage.sample.interactor.ip.network.IpApi
+import ru.surfstudio.android.filestorage.utils.AppDirectoriesProvider
 
 @Module
 class IpModule {
 
     @Provides
     @PerApplication
-    fun provideIpApi(retrofit: Retrofit): IpApi = retrofit.create(IpApi::class.java)
+    internal fun provideIpStorage(appDirectoriesProvider: AppDirectoriesProvider): IpStorage {
+        return IpStorage(appDirectoriesProvider.provideInternalCacheDir())
+    }
+
+    @Provides
+    @PerApplication
+    internal fun provideIpApi(retrofit: Retrofit): IpApi = retrofit.create(IpApi::class.java)
 }
