256acc2e38b6d75444f9faf46a14dee1e9bfeef2
Margarita Volodina <volodina@surfstudio.co>
2018-09-25 19:12:26 +0300

    ANDDEP-273 Исправлен network-sample

---
+++ network-sample/src/main/java/ru/surfstudio/android/network/sample/interactor/common/network/EtagModule.kt
@@ -0,0 +1,29 @@
+package ru.surfstudio.android.network.sample.interactor.common.network
+
+import dagger.Module
+import dagger.Provides
+import ru.surfstudio.android.dagger.scope.PerApplication
+import ru.surfstudio.android.filestorage.utils.AppDirectoriesProvider
+import ru.surfstudio.android.network.etag.EtagInterceptor
+import ru.surfstudio.android.network.etag.storage.EtagCache
+import ru.surfstudio.android.network.etag.storage.EtagStorage
+
+@Module
+class EtagModule {
+
+    @Provides
+    @PerApplication
+    internal fun provideEtagCache(appDirectoriesProvider: AppDirectoriesProvider): EtagCache {
+        return EtagCache(appDirectoriesProvider.provideInternalCacheDir())
+    }
+
+    @Provides
+    @PerApplication
+    internal fun provideEtagStorage(etagCache: EtagCache): EtagStorage = EtagStorage(etagCache)
+
+    @Provides
+    @PerApplication
+    internal fun provideEtagInterceptor(etagStorage: EtagStorage): EtagInterceptor {
+        return EtagInterceptor(etagStorage)
+    }
+}
