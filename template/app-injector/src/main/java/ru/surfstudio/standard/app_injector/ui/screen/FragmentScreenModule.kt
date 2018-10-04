12682535243d83a8971e9242411476e96265737a
Valeriy Shtaits <bajiepkasmile@yandex.ru>
2018-10-04 14:14:23 +0300

    ANDDEP-248 Фикс зависимостей

--- template/app-injector/src/main/java/ru/surfstudio/standard/app_injector/ui/screen/FragmentScreenModule.kt
+++ template/app-injector/src/main/java/ru/surfstudio/standard/app_injector/ui/screen/FragmentScreenModule.kt
@@ -1,5 +1,6 @@
 package ru.surfstudio.standard.app_injector.ui.screen
 
+import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import ru.surfstudio.android.core.mvp.scope.FragmentViewPersistentScope
@@ -18,7 +19,9 @@ import ru.surfstudio.android.message.DefaultMessageController
 import ru.surfstudio.android.message.MessageController
 import ru.surfstudio.android.mvp.dialog.navigation.navigator.DialogNavigator
 import ru.surfstudio.android.mvp.dialog.navigation.navigator.DialogNavigatorForFragment
+import ru.surfstudio.android.shared.pref.NO_BACKUP_SHARED_PREF
 import ru.surfstudio.standard.app_injector.ui.error.ErrorHandlerModule
+import javax.inject.Named
 
 @Module(includes = [ErrorHandlerModule::class])
 class FragmentScreenModule(private val persistentScope: FragmentViewPersistentScope) : ScreenModule() {
@@ -43,10 +46,18 @@ class FragmentScreenModule(private val persistentScope: FragmentViewPersistentSc
 
     @Provides
     @PerScreen
-    internal fun providePermissionManager(activityProvider: ActivityProvider,
-                                          fragmentProvider: FragmentProvider,
-                                          eventDelegateManager: ScreenEventDelegateManager): PermissionManager {
-        return PermissionManagerForFragment(activityProvider, fragmentProvider, eventDelegateManager)
+    internal fun providePermissionManager(eventDelegateManager: ScreenEventDelegateManager,
+                                          activityProvider: ActivityProvider,
+                                          activityNavigator: ActivityNavigator,
+                                          @Named(NO_BACKUP_SHARED_PREF) sharedPreferences: SharedPreferences,
+                                          fragmentProvider: FragmentProvider): PermissionManager {
+        return PermissionManagerForFragment(
+                eventDelegateManager,
+                activityProvider,
+                activityNavigator,
+                sharedPreferences,
+                fragmentProvider
+        )
     }
 
     @Provides
