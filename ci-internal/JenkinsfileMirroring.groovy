c41f3cc1ad39bc38e9e5e4f2c68c67d97148234a
Максим Туев <tuev@surfstudio.ru>
2018-10-07 07:14:51 +0000

    add mirroring logic

--- ci-internal/JenkinsfileMirroring.groovy
+++ ci-internal/JenkinsfileMirroring.groovy
@@ -1,5 +1,4 @@
 @Library('surf-lib@version-1.0.0-SNAPSHOT') // https://bitbucket.org/surfstudio/jenkins-pipeline-lib/
-import ru.surfstudio.ci.pipeline.PrPipelineAndroid
 import ru.surfstudio.ci.pipeline.empty.EmptyScmPipeline
 import ru.surfstudio.ci.stage.StageStrategy
 
@@ -7,13 +6,31 @@ import ru.surfstudio.ci.stage.StageStrategy
 def pipeline = new EmptyScmPipeline(this)
 
 //configuration
-def mirrorRepoUrl = "https://github.com/surfstudio/SurfAndroidStandard.git"
 def mirrorRepoCredentialID = "76dbac13-e6ea-4ed0-a013-e06cad01be2d"
+
+pipeline.propertiesProvider = {
+    return [
+        pipelineTriggers([
+            GenericTrigger()
+        ])
+    ]
+}
+
 
 //stages
 pipeline.stages = [
+        pipeline.createStage("CLONE", StageStrategy.FAIL_WHEN_STAGE_ERROR) {
+            sh "rm -rf android-standard"
+            withCredentials([usernamePassword(credentialsId: pipeline.repoCredentialsId, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                sh "git clone --mirror https://$USERNAME:$PASSWORD@bitbucket.org/surfstudio/android-standard/"
+            }
+        },
         pipeline.createStage("MIRRORING", StageStrategy.FAIL_WHEN_STAGE_ERROR) {
-            //todo
+            dir("android-standard") {
+                withCredentials([usernamePassword(credentialsId: mirrorRepoCredentialID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                    sh "git push --mirror https://$USERNAME:$PASSWORD@github.com/surfstudio/SurfAndroidStandard.git"
+                }
+            }
         }
 ]
 
