cafdff9148edcad19ee21132f3bebd6fa01f1dd6
Eugene Saturov <saturov@surfstudio.ru>
2018-10-11 10:07:03 +0300

    Миграция на AndroidX - финальная стадия

--- app-migration-sample/src/main/java/ru/surfstudio/android/app/migration/sample/ui/screen/splash/SplashActivityView.kt
+++ app-migration-sample/src/main/java/ru/surfstudio/android/app/migration/sample/ui/screen/splash/SplashActivityView.kt
@@ -1,6 +1,6 @@
 package ru.surfstudio.android.app.migration.sample.ui.screen.splash
 
-import android.support.annotation.LayoutRes
+import androidx.annotation.LayoutRes
 import ru.surfstudio.android.app.migration.sample.R
 import ru.surfstudio.android.app.migration.sample.ui.base.configurator.CustomActivityScreenConfigurator
 import ru.surfstudio.android.core.mvp.activity.BaseRenderableActivityView
