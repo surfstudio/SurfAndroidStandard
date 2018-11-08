8a1e9c75e11fa8025461ab3862541f2eb85f3ca3
Margarita Volodina <volodina@surfstudio.co>
2018-11-08 19:08:55 +0300

    ANDDEP-324 Начало создания теста для custom-view-sample

---
+++ custom-view-sample/src/androidTest/java/ru/surfstudio/android/custom_view_sample/CustomViewSampleTest.kt
@@ -0,0 +1,23 @@
+package ru.surfstudio.android.custom_view_sample
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.surfstudio.android.sample.common.test.utils.launchActivity
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class CustomViewSampleTest {
+
+    @Before
+    fun setUp() {
+        launchActivity(MainActivity::class.java)
+    }
+
+    @Test
+    fun testCustomViw() {
+
+    }
+}
