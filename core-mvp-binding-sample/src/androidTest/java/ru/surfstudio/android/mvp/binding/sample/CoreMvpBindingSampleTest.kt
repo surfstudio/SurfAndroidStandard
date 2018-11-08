d242c60c7cfa176f94f2bb2924cdeabe3f4a1f89
Margarita Volodina <volodina@surfstudio.co>
2018-11-08 18:24:24 +0300

    ANDDEP-324 Добавлен тест для core-mvp-binding-sample

---
+++ core-mvp-binding-sample/src/androidTest/java/ru/surfstudio/android/mvp/binding/sample/CoreMvpBindingSampleTest.kt
@@ -0,0 +1,40 @@
+package ru.surfstudio.android.mvp.binding.sample
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.surfstudio.android.core.mvp.binding.sample.R
+import ru.surfstudio.android.mvp.binding.sample.ui.screen.main.MainActivityView
+import ru.surfstudio.android.sample.common.test.utils.checkIfToastIsVisible
+import ru.surfstudio.android.sample.common.test.utils.performClick
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class CoreMvpBindingSampleTest {
+
+    @Before
+    fun launchActivity() {
+        ActivityScenario.launch(MainActivityView::class.java)
+    }
+
+    @Test
+    fun testClickActions() {
+        performClick(
+                "1",
+                R.id.pane_1,
+                R.id.pane_2,
+                R.id.pane_3,
+                R.id.pane_4,
+                R.id.pane_5,
+                R.id.pane_6,
+                R.id.pane_7,
+                R.id.pane_8,
+                R.id.pane_9
+        )
+        performClick(R.id.easy_win_btn)
+        checkIfToastIsVisible(R.string.win_message)
+    }
+}
--- sample-common/src/main/java/ru/surfstudio/android/sample/common/test/utils/TestUtils.kt
+++ sample-common/src/main/java/ru/surfstudio/android/sample/common/test/utils/TestUtils.kt
@@ -1,15 +1,62 @@
 package ru.surfstudio.android.sample.common.test.utils
 
 import androidx.annotation.IdRes
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.annotation.StringRes
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 
 /**
  * Функция, выполняющая нажатие на каждую вью, id которых переданы в параметрах
  */
 fun performClick(@IdRes vararg viewIdResList: Int) {
     viewIdResList.forEach {
-        Espresso.onView(ViewMatchers.withId(it)).perform(ViewActions.click())
+        onView(withId(it)).perform(click())
     }
+}
+
+/**
+ * Функция, выполняющая нажатие на каждую вью, id которых переданы в параметрах,
+ * и проверяющая, что значение каждой вью после клика равно указанному тексту
+ */
+fun performClick(textForCheck: String, @IdRes vararg viewIdResList: Int) {
+    viewIdResList.forEach {
+        onView(withId(it))
+                .perform(click())
+                .check(matches(withText(textForCheck)))
+    }
+}
+
+/**
+ * Функция, проверяющая, что text или hint для вью равны значению строкового ресурса,
+ * и выполняющая ввод нового значения для вью.
+ *
+ * @param viewResId id вью, значения которой будут проверено, а затем изменено
+ * @param oldTextResId id строкового ресурса, значение которого должно быть равно hint или text для вью
+ * @param isHint флаг, указывающий, какое свойство для вью требуется проверить - hint или text
+ * @param newText новое значение текста для вью
+ */
+fun checkAndInputText(
+        @IdRes viewResId: Int,
+        @StringRes oldTextResId: Int,
+        isHint: Boolean,
+        newText: String
+) {
+    onView(withId(viewResId))
+            .check(matches(
+                    if (isHint)
+                        withHint(oldTextResId)
+                    else
+                        withText(oldTextResId)))
+            .perform(typeText(newText), closeSoftKeyboard())
+}
+
+/**
+ * Функция, проверяющая, что на экране отображается Toast с заданным сообщением
+ */
+fun checkIfToastIsVisible(@StringRes toastResId: Int) {
+    onView(withText(toastResId))
+            .inRoot(ToastMatcher())
+            .check(matches(isDisplayed()))
 }
