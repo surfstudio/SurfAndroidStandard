7c994f98e49b7b7aa05f7afce103d5e437a86280
Margarita <margo.himera@yandex.ru>
2018-11-11 14:02:46 +0300

    ANDDEP-324 Завершен тест для mvp-dialog-sample

--- mvp-dialog-sample/src/androidTest/java/ru/surfstudio/android/mvp/dialog/sample/MvpDialogSampleTest.kt
+++ mvp-dialog-sample/src/androidTest/java/ru/surfstudio/android/mvp/dialog/sample/MvpDialogSampleTest.kt
@@ -21,26 +21,19 @@ class MvpDialogSampleTest {
 
     @Test
     fun testMvpDialogSample() {
-        //test simple dialog showing
         testSimpleDialog(
                 R.id.show_simple_dialog_btn,
                 intArrayOf(R.string.simple_dialog_title, R.string.simple_dialog_message),
                 android.R.id.button1
         )
 
-        //test simple bottom sheet dialog
         testSimpleDialog(
                 R.id.show_simple_bottomsheet_dialog_btn,
                 intArrayOf(R.string.bottom_sheet_action),
                 R.id.simple_bottom_sheet_dialog_action_container
         )
 
-        //test complex dialog showing
-        performClick(R.id.show_complex_dialog_btn)
-        checkViewText(R.id.value_tv, INITIAL_COMPLEX_DIALOG_VALUE.toString())
-        clickAndCheck(R.id.decrease_btn, INITIAL_COMPLEX_DIALOG_VALUE - 1)
-        clickAndCheck(R.id.increase_btn, INITIAL_COMPLEX_DIALOG_VALUE)
-        performClick(R.id.apply_btn)
+        testComplexDialog(R.id.show_complex_dialog_btn, R.id.show_complex_bottomsheet_dialog_btn)
     }
 
     private fun testSimpleDialog(
@@ -53,8 +46,22 @@ class MvpDialogSampleTest {
         performClick(acceptDialogBtnResId)
     }
 
+    private fun testComplexDialog(@IdRes vararg showSimpleDialogBtnResIdList: Int) {
+        showSimpleDialogBtnResIdList.forEach {
+            performClick(it)
+            checkViewText(INITIAL_COMPLEX_DIALOG_VALUE)
+            clickAndCheck(R.id.decrease_btn, INITIAL_COMPLEX_DIALOG_VALUE - 1)
+            clickAndCheck(R.id.increase_btn, INITIAL_COMPLEX_DIALOG_VALUE)
+            performClick(R.id.apply_btn)
+        }
+    }
+
     private fun clickAndCheck(@IdRes buttonResId: Int, newValue: Int) {
         performClick(buttonResId)
-        checkViewText(R.id.value_tv, newValue.toString())
+        checkViewText(newValue)
+    }
+
+    private fun checkViewText(value: Int) {
+        checkViewText(R.id.value_tv, value.toString())
     }
 }
