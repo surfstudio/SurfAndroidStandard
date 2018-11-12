0d0b5a6ed8e1a99bc0880aadf06b65e1b9d899f8
Margarita Volodina <volodina@surfstudio.co>
2018-11-12 14:29:01 +0300

    ANDDEP-324 Рефакторинг + добавлен тест для shared-pref-sample

---
+++ shared-pref-sample/src/androidTest/java/ru/surfstudio/android/shared/pref/sample/SharedPrefSample.kt
@@ -0,0 +1,18 @@
+package ru.surfstudio.android.shared.pref.sample
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.surfstudio.android.sample.common.test.utils.launchActivity
+import ru.surfstudio.android.shared.pref.sample.ui.screen.main.MainActivityView
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class SharedPrefSample {
+
+    @Test
+    fun testSharedPrefSample() {
+        launchActivity(MainActivityView::class.java)
+    }
+}
