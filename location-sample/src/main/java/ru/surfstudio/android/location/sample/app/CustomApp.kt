51b1a162eb6bc91b070f69ccec43379e87bac493
Margarita Volodina <volodina@surfstudio.co>
2018-11-16 14:46:17 +0300

    ANDDEP-340 Исправлен location-sample

---
+++ location-sample/src/main/java/ru/surfstudio/android/location/sample/app/CustomApp.kt
@@ -0,0 +1,25 @@
+package ru.surfstudio.android.location.sample.app
+
+import ru.surfstudio.android.core.app.CoreApp
+import ru.surfstudio.android.sample.dagger.app.dagger.DefaultAppModule
+import ru.surfstudio.android.location.sample.app.dagger.CustomAppComponent
+import ru.surfstudio.android.location.sample.app.dagger.DaggerCustomAppComponent
+
+/**
+ * Класс приложения
+ */
+class CustomApp : CoreApp() {
+
+    var customAppComponent: CustomAppComponent? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        initInjector()
+    }
+
+    private fun initInjector() {
+        customAppComponent = DaggerCustomAppComponent.builder()
+                .defaultAppModule(DefaultAppModule(this))
+                .build()
+    }
+}
