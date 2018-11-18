8ede1710fb6e577205756e97c3f8ae173f77ba2b
Vita Sokolova <vsokolova@surfstudio.co>
2018-11-18 21:38:14 +0300

    ANDDEP-320 мелкие правки

--- loadstate-sample/src/main/java/ru/surfstudio/android/loadstate/sample/ui/base/loadstate/presentations/ErrorLoadStateWithStubsPresentation.kt
+++ loadstate-sample/src/main/java/ru/surfstudio/android/loadstate/sample/ui/base/loadstate/presentations/ErrorLoadStateWithStubsPresentation.kt
@@ -5,7 +5,6 @@ import ru.surfstudio.android.core.mvp.model.state.LoadStateInterface
 import ru.surfstudio.android.easyadapter.EasyAdapter
 import ru.surfstudio.android.easyadapter.ItemList
 import ru.surfstudio.android.loadstate.sample.ui.base.loadstate.presentations.controllers.StubData
-import ru.surfstudio.android.recycler.extension.add
 import ru.surfstudio.android.loadstate.sample.ui.base.loadstate.states.ErrorLoadState
 import ru.surfstudio.android.loadstate.sample.ui.base.loadstate.presentations.controllers.StubLoadStateController
 
@@ -29,6 +28,5 @@ class ErrorLoadStateWithStubsPresentation(private val adapter: EasyAdapter) : Lo
     }
 
     override fun hidePresentation(loadStateFrom: ErrorLoadState, loadStateTo: LoadStateInterface) {
-//        errorLoadStateController.resetState()
     }
 }
