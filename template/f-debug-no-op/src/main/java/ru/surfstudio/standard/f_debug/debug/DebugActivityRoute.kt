75425a24bb34f370aa013590a2d6094ca71a2576
Axbor Axrorov <ahrorov@surfstudio.ru>
2018-11-19 19:02:39 +0500

    ANDDEP-327 оптимизация

--- template/f-debug-no-op/src/main/java/ru/surfstudio/standard/f_debug/debug/DebugActivityRoute.kt
+++ template/f-debug-no-op/src/main/java/ru/surfstudio/standard/f_debug/debug/DebugActivityRoute.kt
@@ -1,9 +1,5 @@
 package ru.surfstudio.standard.f_debug.debug
 
-import android.content.Context
-import android.content.Intent
 import ru.surfstudio.android.core.ui.navigation.Route
 
-class DebugActivityRoute : Route {
-    fun prepareIntent(context: Context): Intent = Intent()
-}
+class DebugActivityRoute : Route
