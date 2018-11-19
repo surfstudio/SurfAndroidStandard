c41c0b939de4ed18654160663e2077cd8b1ad995
trushchinskii-surf <trushchinskii@surfstudio.ru>
2018-11-19 18:40:52 +0300

    ANDDEP-270 добавлен дополнительный конструктор с готовой stub-view

--- easyadapter/src/main/java/ru/surfstudio/android/easyadapter/holder/async/AsyncBindableViewHolder.kt
+++ easyadapter/src/main/java/ru/surfstudio/android/easyadapter/holder/async/AsyncBindableViewHolder.kt
@@ -1,18 +1,20 @@
 package ru.surfstudio.android.easyadapter.holder.async
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import ru.surfstudio.android.easyadapter.R
 import ru.surfstudio.android.easyadapter.holder.BindableViewHolder
 
 /**
- * Имплементация BindableViewHolder для асинхроного инфлейта layoutId
+ * Имплементация BindableViewHolder для асинхроного инфлейта
  */
 abstract class AsyncBindableViewHolder<T> private constructor(
         parent: ViewGroup,
         containerWidth: Int,
         containerHeight: Int
 ) : BindableViewHolder<T>(getContainer(parent, containerWidth, containerHeight)), AsyncViewHolder {
+
     final override var isItemViewInflated = false
     final override var fadeInDuration = DEFAULT_FADE_IN_DURATION
 
@@ -32,8 +34,21 @@ abstract class AsyncBindableViewHolder<T> private constructor(
         }
     }
 
+    constructor(parent: ViewGroup,
+                @LayoutRes layoutId: Int,
+                stubView: View,
+                containerWidth: Int = DEFAULT_WIDTH,
+                containerHeight: Int = DEFAULT_HEIGHT
+    ) : this(parent, containerWidth, containerHeight) {
+        (itemView as ViewGroup).addView(stubView)
+        inflateItemView(itemView, layoutId) {
+            if (isBindExecuted) bind(data)
+        }
+    }
+
     final override fun bind(data: T?) {
         isBindExecuted = true
+
         this.data = data
         if (isItemViewInflated) {
             bindInternal(data)
--- easyadapter/src/main/java/ru/surfstudio/android/easyadapter/holder/async/AsyncDoubleBindableViewHolder.kt
+++ easyadapter/src/main/java/ru/surfstudio/android/easyadapter/holder/async/AsyncDoubleBindableViewHolder.kt
@@ -1,18 +1,20 @@
 package ru.surfstudio.android.easyadapter.holder.async
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import ru.surfstudio.android.easyadapter.R
 import ru.surfstudio.android.easyadapter.holder.DoubleBindableViewHolder
 
 /**
- * Имплементация DoubleBindableViewHolder для асинхроного инфлейта layoutId
+ * Имплементация DoubleBindableViewHolder для асинхроного инфлейта
  */
 abstract class AsyncDoubleBindableViewHolder<T1, T2> private constructor(
         parent: ViewGroup,
         containerWidth: Int,
         containerHeight: Int
 ) : DoubleBindableViewHolder<T1, T2>(getContainer(parent, containerWidth, containerHeight)), AsyncViewHolder {
+
     final override var isItemViewInflated = false
     final override var fadeInDuration = DEFAULT_FADE_IN_DURATION
 
@@ -33,10 +35,23 @@ abstract class AsyncDoubleBindableViewHolder<T1, T2> private constructor(
         }
     }
 
+    constructor(parent: ViewGroup,
+                @LayoutRes layoutId: Int,
+                stubView: View,
+                containerWidth: Int = DEFAULT_WIDTH,
+                containerHeight: Int = DEFAULT_HEIGHT
+    ) : this(parent, containerWidth, containerHeight) {
+        (itemView as ViewGroup).addView(stubView)
+        inflateItemView(itemView, layoutId) {
+            if (isBindExecuted) bind(firstData, secondData)
+        }
+    }
+
     final override fun bind(firstData: T1?, secondData: T2?) {
+        isBindExecuted = true
+
         this.firstData = firstData
         this.secondData = secondData
-        isBindExecuted = true
         if (isItemViewInflated) bindInternal(firstData, secondData)
     }
 
