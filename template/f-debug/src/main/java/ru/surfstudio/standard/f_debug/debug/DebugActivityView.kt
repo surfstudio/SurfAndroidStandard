72dc38e24259a391762ee4dd698c4a9d6a807374
trushchinskii-surf <trushchinskii@surfstudio.ru>
2018-11-20 13:20:26 +0300

    ANDDEP-336 замена контекста активити на контекст приложения

--- template/f-debug/src/main/java/ru/surfstudio/standard/f_debug/debug/DebugActivityView.kt
+++ template/f-debug/src/main/java/ru/surfstudio/standard/f_debug/debug/DebugActivityView.kt
@@ -45,6 +45,6 @@ class DebugActivityView : BaseRenderableActivityView<DebugScreenModel>() {
         show_fcm_token_item_layout.setOnClickListener { presenter.openFcmTokenScreen() }
         show_memory_item_layout.setOnClickListener { presenter.openMemoryScreen() }
         show_app_info_item_layout.setOnClickListener { presenter.openAppInfoScreen() }
-        show_fps_item_layout.setOnClickListener { TinyDancer.create().show(this); }
+        show_fps_item_layout.setOnClickListener { TinyDancer.create().show(applicationContext); }
     }
 }
