88151750602552e5935dce38ec95ecc808cb4257
makstuev <maks.tuev@gmail.com>
2018-11-21 16:03:45 +0300

    auto increment version

--- ci-internal/JenkinsfileDeployJob.groovy
+++ ci-internal/JenkinsfileDeployJob.groovy
@@ -13,7 +13,6 @@ import java.util.regex.Pattern
 import java.util.regex.Matcher
 
 import static ru.surfstudio.ci.CommonUtil.applyParameterIfNotEmpty
-
 //Кастомный пайплайн для деплоя артефактов
 
 // Имена Шагов
@@ -23,7 +22,13 @@ def BUILD = 'Build'
 def UNIT_TEST = 'Unit Test'
 def INSTRUMENTATION_TEST = 'Instrumentation Test'
 def STATIC_CODE_ANALYSIS = 'Static Code Analysis'
+def VERSION_INCREASE = 'Version Increase'
 def DEPLOY = 'Deploy'
+def VERSION_PUSH = 'Version Push'
+
+//constants
+def gradleConfigFile = "config.gradle"
+def androidStandardVersionVarName = "moduleVersionName"
 
 //init
 def script = this
@@ -49,20 +54,23 @@ pipeline.initializeBody = {
     //Выбираем значения веток из параметров, Установка их в параметры происходит
     // если триггером был webhook или если стартанули Job вручную
     //Используется имя branchName_0 из за особенностей jsonPath в GenericWebhook plugin
-    applyParameterIfNotEmpty(script, 'branchName', script.params.branchName_0, {
+    CommonUtil.extractValueFromEnvOrParamsAndRun(script, 'branchName_0') {
         value -> branchName = value
-    })
+    }
 
-    if(branchName.contains("project-snapshot")){ //todo do not ignore stages fore project-release build (нжно парсить config файл и смотреть на постфикс SNAPSHOT)
-        script.echo "Apply lightweight strategies for project-snapshot branch"
+    if(isProjectSnapshotBranch(branchName)){
+        script.echo "Apply lightweight strategies and automatic version increment for project-snapshot branch"
         pipeline.getStage(BUILD).strategy = StageStrategy.SKIP_STAGE
         pipeline.getStage(UNIT_TEST).strategy = StageStrategy.SKIP_STAGE
         pipeline.getStage(INSTRUMENTATION_TEST).strategy = StageStrategy.SKIP_STAGE
         pipeline.getStage(STATIC_CODE_ANALYSIS).strategy = StageStrategy.SKIP_STAGE
+        pipeline.getStage(VERSION_INCREASE).strategy = StageStrategy.FAIL_WHEN_STAGE_ERROR
+        pipeline.getStage(VERSION_PUSH).strategy = StageStrategy.FAIL_WHEN_STAGE_ERROR
     }
 
     CommonUtil.safe(script){
-        JarvisUtil.sendMessageToGroup(script, "Инициирован Deploy ветки ${branchName}", pipeline.repoUrl, "bitbucket", true)
+        def buildLink = CommonUtil.getBuildUrlMarkdownLink(script)
+        JarvisUtil.sendMessageToGroup(script, "Инициирован Deploy ветки ${branchName}. $buildLink", pipeline.repoUrl, "bitbucket", true)
     }
 
     def buildDescription = branchName
@@ -81,8 +89,11 @@ pipeline.stages = [
             RepositoryUtil.saveCurrentGitCommitHash(script)
         },
         pipeline.createStage(CHECK_BRANCH_AND_VERSION, StageStrategy.FAIL_WHEN_STAGE_ERROR){
-            def rawVersion = AndroidUtil.getGradleVariable(script, "config.gradle", "moduleVersionName")
-            def version = rawVersion.substring(1, rawVersion.length()-1) //remove quotes
+            if (RepositoryUtil.isCurrentCommitMessageContainsSkipCi(script) && !CommonUtil.isJobStartedByUser(script)){
+                throw new InterruptedException("Job aborted, because it triggered automatically and last commit contains [skip ci] label")
+            }
+            def rawVersion = AndroidUtil.getGradleVariable(script, gradleConfigFile, androidStandardVersionVarName)
+            def version = CommonUtil.removeQuotesFromTheEnds(rawVersion) //remove quotes
             def branch = branchName
             def originPrefix = "origin/"
             if(branch.contains(originPrefix)){
@@ -122,8 +133,23 @@ pipeline.stages = [
         pipeline.createStage(STATIC_CODE_ANALYSIS, StageStrategy.SKIP_STAGE) {
             AndroidPipelineHelper.staticCodeAnalysisStageBody(script)
         },
+        pipeline.createStage(VERSION_INCREASE, StageStrategy.SKIP_STAGE) {
+            if(isProjectSnapshotBranch(branchName)){
+                def rawVersion = AndroidUtil.getGradleVariable(script, gradleConfigFile, androidStandardVersionVarName)
+                String[] versionParts = rawVersion.split("-")
+                String newSnapshotVersion = String.valueOf(Integer.parseInt(versionParts[2]) + 1)
+                newRawVersion = versionParts[0] + versionParts[1] + newSnapshotVersion + versionParts[3]
+                AndroidUtil.changeGradleVariable(script, gradleConfigFile, androidStandardVersionVarName, newRawVersion)
+            }
+        },
         pipeline.createStage(DEPLOY, StageStrategy.FAIL_WHEN_STAGE_ERROR) {
             script.sh "./gradlew clean uploadArchives"
+        },
+        pipeline.createStage(VERSION_PUSH, StageStrategy.SKIP_STAGE) {
+            def version = CommonUtil.removeQuotesFromTheEnds(
+                    AndroidUtil.getGradleVariable(script, gradleConfigFile, androidStandardVersionVarName))
+            script.sh "git commit -a -m \"[skip ci] Increase version to $version\""
+            script.sh "git push"
         }
 ]
 
@@ -146,6 +172,10 @@ pipeline.run()
 
 // UTILS
 
+def static isProjectSnapshotBranch(branchName) {
+    branchName.contains("project-snapshot")
+}
+
 def boolean checkVersionAndBranch(Object script, String branch, String branchRegex, String version, String versionRegex) {
     Pattern branchPattern = Pattern.compile(branchRegex);
     Matcher branchMatcher =  branchPattern.matcher(branch);
@@ -155,7 +185,7 @@ def boolean checkVersionAndBranch(Object script, String branch, String branchReg
         if (versionMatcher.matches()) {
             return true
         } else {
-            script.error("Deploy version: '$version' from branch: '$branch' forbidden")
+            script.error("Deploy version: '$version' from branch: '$branch' forbidden, it must corresponds to $versionRegex")
         }
     }
     return false
@@ -167,13 +197,13 @@ def boolean checkVersionAndBranchForProjectSnapshot(Object script, String branch
     Matcher branchMatcher =  branchPattern.matcher(branch);
     if(branchMatcher.matches()) {
         def projectKey = branch.split('-')[2]
-        def versionRegex = /^\d{1,4}\.\d{1,4}\.\d{1,4}-$projectKey-(\d{1,4}\.\d{1,4}\.\d{1,4}-SNAPSHOT|\d{1,4}\.\d{1,4}\.\d{1,4}|SNAPSHOT)$/
+        def versionRegex = /^\d{1,4}\.\d{1,4}\.\d{1,4}-$projectKey-\d{1,4}-SNAPSHOT$/
         Pattern versionPattern = Pattern.compile(versionRegex);
         Matcher versionMatcher =  versionPattern.matcher(version);
         if (versionMatcher.matches()) {
             return true
         } else {
-            script.error("Deploy version: '$version' from branch: '$branch' forbidden")
+            script.error("Deploy version: '$version' from branch: '$branch' forbidden, it must corresponds to $versionRegex")
         }
     }
     return false
