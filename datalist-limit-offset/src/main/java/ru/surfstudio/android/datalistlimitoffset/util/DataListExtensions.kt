fdb8a65199f2322a1b4a05b34c4cb39ba6ad8841
Mikhail Zinchenko <mzinchenko@surfstudio.co>
2018-11-22 11:29:13 +0300

    ANDDEP-242 Добавление фильтрации и экстеншнов DataList, добавление тестов

---
+++ datalist-limit-offset/src/main/java/ru/surfstudio/android/datalistlimitoffset/util/DataListExtensions.kt
@@ -0,0 +1,34 @@
+package ru.surfstudio.android.datalistlimitoffset.util
+
+import ru.surfstudio.android.datalistlimitoffset.domain.datalist.DataList
+
+/**
+ * Создание пустого DataList
+ *
+ * @param totalCount суммарное количество элементов в новом
+ */
+fun <T> emptyDataListOf(totalCount: Int = 0) = DataList.emptyWithTotal<T>(totalCount)
+
+/**
+ * Фильтрация списка по указанному условию
+ *
+ * @param predicate функция условия фильтрации
+ *
+ * @return отфильтрованный DataList
+ */
+fun <T> DataList<T>.filter(predicate: (T) -> Boolean): DataList<T> {
+    val result = (this as Iterable<T>).filter(predicate)
+    return DataList(result, limit, offset, totalCount)
+}
+
+/**
+ * Трансформация списка
+ *
+ * @param transform функция трансформации элемента
+ *
+ * @return трансформированный DataList
+ */
+fun <T, R> DataList<T>.map(transform: (T) -> R): DataList<R> {
+    val result = (this as Iterable<T>).map(transform)
+    return DataList(result, limit, offset, totalCount)
+}
