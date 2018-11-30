d6736a2b9c9b2b7ebbfeab5b28f42b278a4430e7
Margarita Volodina <volodina@surfstudio.co>
2018-11-30 12:26:50 +0300

    Enable build stage

--- ci-internal/JenkinsfilePullRequestJob.groovy
+++ ci-internal/JenkinsfilePullRequestJob.groovy
@@ -7,7 +7,6 @@ def pipeline = new PrPipelineAndroid(this)
 pipeline.init()
 
 //customization
-pipeline.getStage(pipeline.BUILD).strategy = StageStrategy.SKIP_STAGE
 pipeline.getStage(pipeline.UNIT_TEST).strategy = StageStrategy.SKIP_STAGE
 pipeline.getStage(pipeline.INSTRUMENTATION_TEST).strategy = StageStrategy.UNSTABLE_WHEN_STAGE_ERROR
 pipeline.getStage(pipeline.STATIC_CODE_ANALYSIS).strategy = StageStrategy.SKIP_STAGE
