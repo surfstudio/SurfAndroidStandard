991555decdddc9c2865e4f842523720ea39d174d
Margarita <margo.himera@yandex.ru>
2018-12-20 21:27:13 +0300

    ANDDEP-340 Исправлен app-migration-sample

--- app-migration-sample/src/androidTest/java/ru/surfstudio/android/app/migration/sample/AppMigrationSampleTest.kt
+++ app-migration-sample/src/androidTest/java/ru/surfstudio/android/app/migration/sample/AppMigrationSampleTest.kt
@@ -1,38 +1,19 @@
 package ru.surfstudio.android.app.migration.sample
 
-import androidx.test.espresso.intent.Intents
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.SmallTest
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import ru.surfstudio.android.app.migration.sample.ui.screen.main.MainActivityView
 import ru.surfstudio.android.app.migration.sample.ui.screen.splash.SplashActivityView
 import ru.surfstudio.android.app.migration.sample.ui.screen.splash.TRANSITION_DELAY_MS
 import ru.surfstudio.android.sample.common.test.ElapsedTimeIdlingResource
+import ru.surfstudio.android.sample.common.test.base.BaseSampleTest
 import ru.surfstudio.android.sample.common.test.utils.ActivityUtils.checkIfActivityIsVisible
-import ru.surfstudio.android.sample.common.test.utils.ActivityUtils.launchActivity
 import ru.surfstudio.android.sample.common.test.utils.IdlingUtils.registerIdlingResource
 import ru.surfstudio.android.sample.common.test.utils.IdlingUtils.unregisterIdlingResource
 
-@RunWith(AndroidJUnit4::class)
-@SmallTest
-class AppMigrationSampleTest {
-
-    @Before
-    fun setUp() {
-        Intents.init()
-    }
-
-    @After
-    fun tearDown() {
-        Intents.release()
-    }
+class AppMigrationSampleTest : BaseSampleTest<SplashActivityView>(SplashActivityView::class.java) {
 
     @Test
     fun testApplicationLaunch() {
-        launchActivity(SplashActivityView::class.java)
         checkIfActivityIsVisible(SplashActivityView::class.java)
 
         // Ожидаем, пока показывается splash-экран
@@ -41,10 +22,5 @@ class AppMigrationSampleTest {
 
         checkIfActivityIsVisible(MainActivityView::class.java)
         unregisterIdlingResource(idlingResource)
-    }
-
-    @Test
-    fun testMainActivity() {
-        launchActivity(MainActivityView::class.java)
     }
 }
--- app-migration-sample/src/main/java/ru/surfstudio/android/app/migration/sample/ui/screen/splash/SplashPresenter.kt
+++ app-migration-sample/src/main/java/ru/surfstudio/android/app/migration/sample/ui/screen/splash/SplashPresenter.kt
@@ -1,5 +1,6 @@
 package ru.surfstudio.android.app.migration.sample.ui.screen.splash
 
+import io.reactivex.Completable
 import ru.surfstudio.android.app.migration.sample.app.initialization.InitializeAppInteractor
 import ru.surfstudio.android.app.migration.sample.ui.screen.main.MainActivityRoute
 import ru.surfstudio.android.core.mvp.presenter.BasePresenter
@@ -7,6 +8,8 @@ import ru.surfstudio.android.core.mvp.presenter.BasePresenterDependency
 import ru.surfstudio.android.core.ui.navigation.activity.navigator.ActivityNavigator
 import ru.surfstudio.android.core.ui.navigation.activity.route.ActivityRoute
 import ru.surfstudio.android.dagger.scope.PerScreen
+import ru.surfstudio.android.logger.Logger
+import ru.surfstudio.android.utilktx.ktx.text.EMPTY_STRING
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -17,9 +20,10 @@ const val TRANSITION_DELAY_MS = 2000L
  * Презентер загрузочного экрана
  */
 @PerScreen
-internal class SplashPresenter @Inject constructor(basePresenterDependency: BasePresenterDependency,
-                                                   private val activityNavigator: ActivityNavigator,
-                                                   private val initializeAppInteractor: InitializeAppInteractor
+internal class SplashPresenter @Inject constructor(
+        basePresenterDependency: BasePresenterDependency,
+        private val activityNavigator: ActivityNavigator,
+        private val initializeAppInteractor: InitializeAppInteractor
 ) : BasePresenter<SplashActivityView>(basePresenterDependency) {
 
     private val sm = SplashScreenModel()
@@ -29,18 +33,23 @@ internal class SplashPresenter @Inject constructor(basePresenterDependency: Base
     private val nextRoute: ActivityRoute
         get() = MainActivityRoute()
 
-    override fun onLoad(viewRecreated: Boolean) {
-        super.onLoad(viewRecreated)
-        view.render(sm)
-        if (!viewRecreated) {
-            subscribeIoHandleError(
-                    initializeAppInteractor
-                            .initialize()
-                            .delay(TRANSITION_DELAY_MS, TimeUnit.MILLISECONDS)) {
-                activityNavigator.finishCurrent()
-                openNextScreen()
-            }
-        }
+    override fun onFirstLoad() {
+        super.onFirstLoad()
+
+        val delay = Completable.timer(TRANSITION_DELAY_MS, TimeUnit.MILLISECONDS)
+        val worker = initializeAppInteractor.initialize()
+        val merge = Completable
+                .merge(arrayListOf(delay, worker))
+                .toSingleDefault(EMPTY_STRING)
+
+        subscribeIoHandleError(merge,
+                {
+                    openNextScreen()
+                },
+                {
+                    Logger.e(it)
+                    openNextScreen()
+                })
     }
 
     private fun openNextScreen() {
