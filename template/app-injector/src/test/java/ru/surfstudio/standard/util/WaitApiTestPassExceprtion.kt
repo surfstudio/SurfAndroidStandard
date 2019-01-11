49ebceee56dcff6780772d6800ef8dcd94c3849d
tuev <tuev@surfstudio.ru>
2019-01-11 19:41:32 +0300

    fix error message

--- template/app-injector/src/test/java/ru/surfstudio/standard/util/WaitApiTestPassExceprtion.kt
+++ template/app-injector/src/test/java/ru/surfstudio/standard/util/WaitApiTestPassExceprtion.kt
@@ -4,4 +4,4 @@ import java.lang.RuntimeException
 
 class WaitApiTestPassExceprtion : RuntimeException(
         "Test with annotation @WaitApiTest is finished without exception. " +
-                "If api method is working now, change annotation to @CheckApiTest")
+                "If api method is working now, remove this annotation")
