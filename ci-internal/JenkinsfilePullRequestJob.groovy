aab3857f4b929ba28fc51a7adb25f566e039a274
Маргарита Володина <volodina@surfstudio.co>
2019-02-01 10:45:01 +0000

    JenkinsfilePullRequestJob.groovy edited online with Bitbucket

--- ci-internal/JenkinsfilePullRequestJob.groovy
+++ ci-internal/JenkinsfilePullRequestJob.groovy
@@ -1,4 +1,4 @@
-@Library('surf-lib@new-insrumentation-tests-fixes') // https://bitbucket.org/surfstudio/jenkins-pipeline-lib/
+@Library('surf-lib@get-avd-name-for-adb') // https://bitbucket.org/surfstudio/jenkins-pipeline-lib/
 import ru.surfstudio.ci.pipeline.pr.PrPipelineAndroid
 import ru.surfstudio.ci.stage.StageStrategy
 
