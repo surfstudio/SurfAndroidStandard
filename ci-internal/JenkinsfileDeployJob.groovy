100cbb192d1cb1606ea851e0c8a162cb5115b837
Маргарита Володина <volodina@surfstudio.co>
2019-02-04 10:40:15 +0000

    JenkinsfileDeployJob.groovy edited online with Bitbucket

--- ci-internal/JenkinsfileDeployJob.groovy
+++ ci-internal/JenkinsfileDeployJob.groovy
@@ -154,7 +154,7 @@ pipeline.stages = [
                     "**/test-results/testReleaseUnitTest/*.xml",
                     "app/build/reports/tests/testReleaseUnitTest/")
         },
-        pipeline.createStage(INSTRUMENTATION_TEST, StageStrategy.SKIP_STAGE) {
+        pipeline.createStage(INSTRUMENTATION_TEST, StageStrategy.FAIL_WHEN_STAGE_ERROR) {
             AndroidPipelineHelper.instrumentationTestStageBodyAndroid(
                     script,
                     new AvdConfig(),
