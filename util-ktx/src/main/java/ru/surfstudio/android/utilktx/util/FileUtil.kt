c1361e28fd61c97c974beb49581dc755a546e9ad
maxsmirnov92 <smirnov-m@surfstudio.co>
2019-04-26 16:49:31 +0400

    ANDDEP-476 добавлен getRealPath для общего извлечения пути из Uri

--- util-ktx/src/main/java/ru/surfstudio/android/utilktx/util/FileUtil.kt
+++ util-ktx/src/main/java/ru/surfstudio/android/utilktx/util/FileUtil.kt
@@ -15,11 +15,15 @@
  */
 package ru.surfstudio.android.utilktx.util
 
-import android.app.Activity
+import android.annotation.TargetApi
+import android.content.ContentUris
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import ru.surfstudio.android.logger.Logger
 import java.io.ByteArrayOutputStream
@@ -30,26 +34,92 @@ import java.io.FileNotFoundException
  */
 object FileUtil {
 
-    fun getRealPathFromImage(activity: Activity, uri: String) = getRealPathFromImage(activity, Uri.parse(uri))
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri     The Uri to query.
+     * @author paulburke
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    fun getRealPath(context: Context, uri: Uri): String? {
+
+        val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+
+            } else if (isDownloadsDocument(uri)) {
+
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
+
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }// MediaProvider
+            // DownloadsProvider
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+
+            // Return the remote address
+            return if (isGooglePhotosDocument(uri)) uri.lastPathSegment else getDataColumn(context, uri, null, null)
+
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }// File
+        // MediaStore (and general)
+
+        return null
+    }
+
+    fun getRealPathFromImage(context: Context, uri: String) = getRealPathFromImage(context, Uri.parse(uri))
 
     /**
      * получение реального пути к изображению используя Uri
      */
-    fun getRealPathFromImage(activity: Activity, uri: Uri): String? {
+    fun getRealPathFromImage(context: Context, uri: Uri): String? {
         var realPath: String? = null
         if (isGooglePhotos(uri)) {
             try {
-                val `is` = activity.contentResolver.openInputStream(uri)
+                val `is` = context.contentResolver.openInputStream(uri)
                 if (`is` != null) {
                     val pictureBitmap = BitmapFactory.decodeStream(`is`)
-                    realPath = getRealPath(activity, getImageUri(activity, pictureBitmap))
+                    realPath = getDataColumn(context, getImageUri(context, pictureBitmap))
                 }
             } catch (e: FileNotFoundException) {
                 Logger.e(e)
             }
 
         } else {
-            realPath = getRealPath(activity, uri)
+            realPath = getDataColumn(context, uri)
         }
         return realPath
     }
@@ -65,15 +135,42 @@ object FileUtil {
         return Uri.parse(path)
     }
 
-    private fun getRealPath(activity: Activity, uri: Uri): String? {
-        val cursor = activity.contentResolver.query(uri, null, null, null, null)
+    private fun getDataColumn(context: Context, uri: Uri?, selection: String? = null, selectionArgs: Array<String>? = null): String? {
         var result: String? = null
-        if (cursor != null) {
-            cursor.moveToFirst()
-            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
-            result = cursor.getString(idx)
-            cursor.close()
+        uri?.let {
+            val cursor = context.contentResolver.query(it, arrayOf(MediaStore.Images.ImageColumns.DATA),
+                    selection, selectionArgs, null)
+            if (cursor != null) {
+                cursor.moveToFirst()
+                val idx = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA)
+                result = cursor.getString(idx)
+                cursor.close()
+            }
         }
         return result
     }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private fun isExternalStorageDocument(uri: Uri) = "com.android.externalstorage.documents" == uri.authority
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private fun isDownloadsDocument(uri: Uri): Boolean = "com.android.providers.downloads.documents" == uri.authority
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private fun isMediaDocument(uri: Uri): Boolean = "com.android.providers.media.documents" == uri.authority
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    private fun isGooglePhotosDocument(uri: Uri): Boolean = "com.google.android.apps.photos.content" == uri.authority
 }
