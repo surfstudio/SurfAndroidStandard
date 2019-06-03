1660c268e3f636ef128764750aef87c8bf121a0f
trushchinskii <trushchinskii@surfstudio.ru>
2019-06-03 13:26:53 +0300

    ANDDEP-500 правки pr

---
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/GradleProperties.kt
@@ -0,0 +1,13 @@
+package ru.surfstudio.android.build
+
+/**
+ * Gradle properties
+ */
+object GradleProperties {
+
+    /**
+     * Components list for check standard dependency stability
+     */
+    const val COMPONENTS_TO_CHECK_STANDARD_DEPENDENCIES_STABILITY =
+            "componentsToCheckStandardDependenciesStability"
+}
