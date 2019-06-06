357f52eacab834bfb7666f0073dfb09055c64a0c
Surikova Dasha <dasha.qt@gmail.com>
2019-06-06 17:17:20 +0300

    Добавлена проверка зависимость android standard

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/changed_components/models/DependencyWithVersion.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/changed_components/models/DependencyWithVersion.kt
@@ -1,15 +1,11 @@
 package ru.surfstudio.android.build.tasks.changed_components.models
 
+import java.util.*
+
 /**
  * Represent information about dependency with version
  */
 data class DependencyWithVersion(
         val name: String,
         var version: String
-) {
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is DependencyWithVersion) return false
-        return other.version == this.version
-    }
-}
+)
