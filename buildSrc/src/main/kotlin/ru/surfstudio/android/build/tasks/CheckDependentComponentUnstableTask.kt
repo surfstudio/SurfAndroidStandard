65e77acafda68c9e135c353fed1eaa90f1e7718a
trushchinskii <trushchinskii@surfstudio.ru>
2019-06-06 16:26:04 +0300

    ANDDEP-514 Revert

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckDependentComponentUnstableTask.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckDependentComponentUnstableTask.kt
@@ -1,16 +1,46 @@
-package ru.surfstudio.android.build.tasks
-
+import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import ru.surfstudio.android.build.Components
+import ru.surfstudio.android.build.GradleProperties
+import ru.surfstudio.android.build.exceptions.ComponentNotFoundException
 import ru.surfstudio.android.build.exceptions.DependentComponentsStableException
+import ru.surfstudio.android.build.exceptions.PropertyNotDefineException
+import ru.surfstudio.android.build.model.module.Library
 
 /**
  * Check components libraries depend on parameter component libraries unstable
  */
-open class CheckDependentComponentUnstableTask : BaseCheckDependentComponentTask() {
+open class CheckDependencyForComponentUnstableTask : DefaultTask() {
 
     @TaskAction
     fun check() {
-        val stableLibraries = getDependentComponentLibraryNames(true)
-        if (stableLibraries.isNotEmpty()) throw DependentComponentsStableException(stableLibraries)
+        val componentName = project.property(GradleProperties.COMPONENT) as? String
+                ?: throw PropertyNotDefineException(GradleProperties.COMPONENT)
+        val component = Components.value.find { it.name == componentName }
+                ?: throw ComponentNotFoundException(componentName)
+        val stableLibraries = ArrayList<Library>()
+
+        component.libraries.forEach {
+            checkDependentLibrariesStable(stableLibraries, it.name)
+        }
+
+        if (stableLibraries.isNotEmpty()) {
+            throw DependentComponentsStableException(stableLibraries.map(Library::name))
+        }
+    }
+
+    private fun checkDependentLibrariesStable(stableLibraries: ArrayList<Library>, libName: String) {
+        Components.value.forEach { comp ->
+            comp.libraries.forEach { lib ->
+                lib.androidStandardDependencies.forEach { dep ->
+                    if (libName == dep.name) {
+                        if (comp.stable) {
+                            stableLibraries.add(lib)
+                        }
+                        checkDependentLibrariesStable(stableLibraries, lib.name)
+                    }
+                }
+            }
+        }
     }
 }
