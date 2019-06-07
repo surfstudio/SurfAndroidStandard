974791b93556b8c356c9f10c7a8208ab596f5f8e
trushchinskii <trushchinskii@surfstudio.ru>
2019-06-07 11:22:58 +0300

    ANDDEP-514 рефакторинг

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckComponentStableTask.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckComponentStableTask.kt
@@ -2,11 +2,8 @@ package ru.surfstudio.android.build.tasks
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
-import ru.surfstudio.android.build.Components
-import ru.surfstudio.android.build.GradleProperties
-import ru.surfstudio.android.build.exceptions.ComponentNotFoundException
 import ru.surfstudio.android.build.exceptions.ComponentUnstableException
-import ru.surfstudio.android.build.exceptions.PropertyNotDefineException
+import ru.surfstudio.android.build.utils.getComponent
 
 /**
  * Check component stable
@@ -18,10 +15,8 @@ open class CheckComponentStableTask : DefaultTask() {
      */
     @TaskAction
     fun check() {
-        val componentName = project.property(GradleProperties.COMPONENT) as? String
-                ?: throw PropertyNotDefineException(GradleProperties.COMPONENT)
-        val component = Components.value.find { it.name == componentName }
-                ?: throw ComponentNotFoundException(componentName)
-        if (!component.stable) throw ComponentUnstableException(componentName)
+        val component = project.getComponent()
+
+        if (!component.stable) throw ComponentUnstableException(component.name)
     }
 }
