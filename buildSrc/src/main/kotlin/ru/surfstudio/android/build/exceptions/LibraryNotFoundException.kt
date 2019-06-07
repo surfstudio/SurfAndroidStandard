9b8613af40c5c075110c247df45cf66db60b1cd8
trushchinskii <trushchinskii@surfstudio.ru>
2019-06-07 17:16:47 +0300

    ANDDEP-536 Для всех стабильных артефактов проверять что его as зависимости тоже stable

---
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/exceptions/LibraryNotFoundException.kt
@@ -0,0 +1,10 @@
+package ru.surfstudio.android.build.exceptions
+
+import org.gradle.api.GradleException
+
+/**
+ * Library not found
+ */
+class LibraryNotFoundException(libraryName: String): GradleException(
+        "Library \"$libraryName\" not found"
+)
