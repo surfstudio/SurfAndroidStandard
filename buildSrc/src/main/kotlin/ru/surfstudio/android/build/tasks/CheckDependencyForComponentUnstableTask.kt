6f85d8fb22e61efa17913827e5a3b33eb458e7bf
trushchinskii <trushchinskii@surfstudio.ru>
2019-06-07 11:18:21 +0300

    ANDDEP-513 Правки PR

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckDependencyForComponentUnstableTask.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/CheckDependencyForComponentUnstableTask.kt
@@ -3,11 +3,9 @@ package ru.surfstudio.android.build.tasks
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import ru.surfstudio.android.build.Components
-import ru.surfstudio.android.build.GradleProperties
-import ru.surfstudio.android.build.exceptions.ComponentNotFoundException
 import ru.surfstudio.android.build.exceptions.DependentComponentsStableException
-import ru.surfstudio.android.build.exceptions.PropertyNotDefineException
 import ru.surfstudio.android.build.model.module.Library
+import ru.surfstudio.android.build.utils.getComponent
 
 /**
  * Check components libraries depend on parameter component libraries unstable
@@ -16,10 +14,7 @@ open class CheckDependencyForComponentUnstableTask : DefaultTask() {
 
     @TaskAction
     fun check() {
-        val componentName = project.property(GradleProperties.COMPONENT) as? String
-                ?: throw PropertyNotDefineException(GradleProperties.COMPONENT)
-        val component = Components.value.find { it.name == componentName }
-                ?: throw ComponentNotFoundException(componentName)
+        val component = project.getComponent()
         val stableLibraries = ArrayList<Library>()
 
         component.libraries.forEach {
