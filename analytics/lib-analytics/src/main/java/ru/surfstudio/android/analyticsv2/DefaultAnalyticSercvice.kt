cbb8622351f4bd0f5cbbdcabdff7cadbdd34463e
trushchinskii <trushchinskii@surfstudio.ru>
2019-08-07 14:21:31 +0300

    ANDDEP-540

--- analytics/lib-analytics/src/main/java/ru/surfstudio/android/analyticsv2/DefaultAnalyticSercvice.kt
+++ analytics/lib-analytics/src/main/java/ru/surfstudio/android/analyticsv2/DefaultAnalyticSercvice.kt
@@ -50,7 +50,7 @@ open class DefaultAnalyticService : AnalyticActionPerformerCreator<AnalyticActio
      * Добавить выполнитель действия
      */
     fun addActionPerformer(performer: AnalyticActionPerformer<AnalyticAction>): DefaultAnalyticService {
-        println("1")
+        println("2")
         performers.add(performer)
         return this
     }
