35622915eb7dbbd4b1bc4301423e9b1084427d4f
trushchinskii <trushchinskii@surfstudio.ru>
2019-08-13 16:38:48 +0300

    ANDDEP-531 12

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/artifactory/Artifactory.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/artifactory/Artifactory.kt
@@ -37,7 +37,7 @@ object Artifactory {
      * Check libraries's android standard dependencies exist in artifactory
      */
     fun checkLibrariesStandardDependenciesExisting(component: Component) {
-        component.libraries.forEach(this::checkLibraryStandardDependenciesExisting)
+        component.libraries.forEach { checkLibraryStandardDependenciesExisting(it, component) }
     }
 
     /**
@@ -53,13 +53,15 @@ object Artifactory {
     /**
      * Check library android standard dependencies exist in artifactory
      */
-    private fun checkLibraryStandardDependenciesExisting(library: Library) {
-        library.androidStandardDependencies.forEach { androidStandardDependency ->
+    private fun checkLibraryStandardDependenciesExisting(library: Library, component: Component) {
+        library.androidStandardDependencies
+                .filter { it.component != component }
+                .forEach { androidStandardDependency ->
             if (!isArtifactExists(androidStandardDependency.name, androidStandardDependency.component.projectVersion)) {
                 throw ArtifactNotExistInArtifactoryException(library.name, androidStandardDependency)
             }
             Components.libraries.find { it.name == androidStandardDependency.name }?.let {
-                checkLibraryStandardDependenciesExisting(it)
+                checkLibraryStandardDependenciesExisting(it, component)
             }
         }
     }
