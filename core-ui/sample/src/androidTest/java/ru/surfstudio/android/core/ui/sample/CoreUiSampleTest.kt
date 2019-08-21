50287259fb5a43901f7e5b30213e44039ab6de09
trushchinskii <trushchinskii@surfstudio.ru>
2019-08-21 15:10:57 +0300

    ANDDEP-534 core-ui-sample

--- core-ui/sample/src/androidTest/java/ru/surfstudio/android/core/ui/sample/CoreUiSampleTest.kt
+++ core-ui/sample/src/androidTest/java/ru/surfstudio/android/core/ui/sample/CoreUiSampleTest.kt
@@ -1,6 +1,9 @@
 package ru.surfstudio.android.core.ui.sample
 
+import org.junit.Test
 import ru.surfstudio.android.core.ui.sample.ui.screen.main.MainActivityView
+import ru.surfstudio.android.sample.common.test.base.BaseSampleTest
+import ru.surfstudio.android.sample.common.test.utils.VisibilityUtils.checkIfSnackbarIsVisible
 
 class CoreUiSampleTest : BaseSampleTest<MainActivityView>(MainActivityView::class.java) {
 
