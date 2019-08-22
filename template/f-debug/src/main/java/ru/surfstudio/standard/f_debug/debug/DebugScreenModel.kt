fbbaadd4e1d3864c97b81c81242bd7537458cfc5
trushchinskii <trushchinskii@surfstudio.ru>
2019-08-22 11:59:26 +0300

    ANDDEP-534 template deleted

--- template/f-debug/src/main/java/ru/surfstudio/standard/f_debug/debug/DebugScreenModel.kt
+++
@@ -1,8 +0,0 @@
-package ru.surfstudio.standard.f_debug.debug
-
-import ru.surfstudio.android.core.mvp.model.ScreenModel
-
-/**
- * Модель экрана показа информации для дебага
- */
-class DebugScreenModel : ScreenModel()
