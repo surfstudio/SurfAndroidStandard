320a29b1ba435c6cc1dba20cf9bb2b2a3115d3cd
trushchinskii <trushchinskii@surfstudio.ru>
2019-09-03 13:21:43 +0300

    ANDDEP-655 fix version-plugin

--- android-standard-version-plugin/src/main/java/ru/surfstudio/android/plugin/AndroidStandardVersionExtension.kt
+++ android-standard-version-plugin/src/main/java/ru/surfstudio/android/plugin/AndroidStandardVersionExtension.kt
@@ -2,7 +2,6 @@ package ru.surfstudio.android.plugin
 
 import com.google.gson.GsonBuilder
 import org.gradle.api.GradleException
-import java.io.File
 
 /**
  * Extension for version plugin
@@ -17,7 +16,7 @@ open class AndroidStandardVersionExtension {
         GsonBuilder()
             .create()
             .fromJson(javaClass.classLoader.getResourceAsStream(FILE_NAME).reader(), Array<LibVersion>::class.java)
-            .associate { it.name to it.version }
+            .associate { it.first to it.second }
     }
 
     fun version(name: String): String = androidStandardMap[name]
