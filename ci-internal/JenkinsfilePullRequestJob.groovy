e48f01738c913b94aff72dc0e5400de6528dc561
Andrey Matyushin <andrew.matyushin@gmail.com>
2019-10-16 09:13:51 +0300

    ANDDEP-696 disable notify for checkout stage

--- ci-internal/JenkinsfilePullRequestJob.groovy
+++ ci-internal/JenkinsfilePullRequestJob.groovy
@@ -160,7 +160,7 @@ pipeline.initializeBody = {
 }
 
 pipeline.stages = [
-        pipeline.stage(CHECKOUT) {
+        pipeline.stage(CHECKOUT, false) {
             CommonUtil.safe(script) {
                 script.sh "git reset --merge" //revert previous failed merge
             }
