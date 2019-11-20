c4b7c9a8ceb9a0135304683178b15de8c0a4b4b0
trofimenko-surf <trofimenko@surfstudio.co>
2019-11-20 16:50:02 +0300

    test

--- ci-internal/DevDeployJobJenkinsfile.groovy
+++ ci-internal/DevDeployJobJenkinsfile.groovy
@@ -104,7 +104,7 @@ pipeline.stages = [
 
 
             script.echo "qwerty2"
-            JarvisUtil.sendMessageToUser(script, "spam229 ${branchName}", "trofimenko@surfstudio.ru", "email")
+            JarvisUtil.sendMessageToUser(script, "spam229 ${pipeline.repoUrl}", "trofimenko@surfstudio.ru", "email")
         },
         pipeline.stage(CHECK_BRANCH_AND_VERSION) {
             String globalConfigurationJsonStr = script.readFile(projectConfigurationFile)
