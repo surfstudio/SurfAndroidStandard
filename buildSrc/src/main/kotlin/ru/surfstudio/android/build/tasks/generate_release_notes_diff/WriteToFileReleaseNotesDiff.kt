d5fb4c101c4fa227f123136e1c95353ddeb21e56
trofimenko <trofimenko@surfstudio.co>
2019-11-25 22:04:30 +0300

    test6

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/generate_release_notes_diff/WriteToFileReleaseNotesDiff.kt
+++ buildSrc/src/main/kotlin/ru/surfstudio/android/build/tasks/generate_release_notes_diff/WriteToFileReleaseNotesDiff.kt
@@ -84,9 +84,13 @@ open class WriteToFileReleaseNotesDiff : DefaultTask() {
         addReleaseNoteChange(lineToPrint)
     }
 
-    private fun parseRawDiff(diff: String): List<GitDiff>
-            = SimpleGitDiffParser().parse(diff)
-            .filter { it.line.trim() != EMPTY_STRING }
+    private fun parseRawDiff(diff: String): List<GitDiff> = SimpleGitDiffParser().parse(diff)
+            .filter {
+                it.line.replace(
+                        "[0-9]{1,4}[ ]{0,4}[+-]".toRegex(),
+                        ""
+                ).trim() != EMPTY_STRING
+            }
 
     private fun extractRawDiff(component: Component): String {
         val filePath = ReleaseNotes.getReleaseNotesFilePath(component)
