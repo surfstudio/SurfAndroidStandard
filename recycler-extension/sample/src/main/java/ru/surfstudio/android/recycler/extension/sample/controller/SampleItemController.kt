1ada14aaa1367c9f97efbaf726ce8d5ac9627e68
Margarita <margo.himera@yandex.ru>
2020-03-30 17:34:33 +0300

    ANDDEP-958 Update recycler-extension-sample

--- recycler-extension/sample/src/main/java/ru/surfstudio/android/recycler/extension/sample/controller/SampleItemController.kt
+++ recycler-extension/sample/src/main/java/ru/surfstudio/android/recycler/extension/sample/controller/SampleItemController.kt
@@ -1,7 +1,6 @@
 package ru.surfstudio.android.recycler.extension.sample.controller
 
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import ru.surfstudio.android.easyadapter.controller.BindableItemController
 import ru.surfstudio.android.easyadapter.holder.BindableViewHolder
@@ -16,20 +15,20 @@ class SampleItemController(
 
     override fun createViewHolder(parent: ViewGroup?): Holder = Holder(parent)
 
-    inner class Holder(parent: ViewGroup?) : BindableViewHolder<Data>(parent, R.layout.item_list_sample) {
+    inner class Holder(
+            parent: ViewGroup?
+    ) : BindableViewHolder<Data>(parent, R.layout.item_list_sample) {
 
         private var textView: TextView = itemView.findViewById(R.id.item_list_sample_tv)
-        private var container: LinearLayout = itemView.findViewById(R.id.container)
+
+        init {
+            if (isFullScreen) {
+                textView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+            }
+        }
 
         override fun bind(data: Data?) {
             textView.text = data?.name
-
-            (container.layoutParams as LinearLayout.LayoutParams).apply {
-                height = if (isFullScreen)
-                    LinearLayout.LayoutParams.MATCH_PARENT
-                else
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-            }
         }
     }
 }
