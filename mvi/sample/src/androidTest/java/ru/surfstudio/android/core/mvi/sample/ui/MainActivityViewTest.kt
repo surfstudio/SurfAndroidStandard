f426e5c6be6e41c14cd13907bda4ebef96be6521
Ilia Trofimenko <trofimenko@surfstudio.ru>
2020-04-01 18:50:52 +0300

    ANDDEP-991 added method checkIfSnackbarIsVisible(String)

--- mvi/sample/src/androidTest/java/ru/surfstudio/android/core/mvi/sample/ui/MainActivityViewTest.kt
+++ mvi/sample/src/androidTest/java/ru/surfstudio/android/core/mvi/sample/ui/MainActivityViewTest.kt
@@ -14,8 +14,6 @@ import ru.surfstudio.android.core.mvi.sample.ui.screen.simple_list.SimpleListAct
 import ru.surfstudio.android.sample.common.test.base.BaseSampleTest
 import ru.surfstudio.android.sample.common.test.utils.ActivityUtils.checkIfActivityIsVisible
 import ru.surfstudio.android.sample.common.test.utils.RecyclerViewUtils.performItemClick
-import ru.surfstudio.android.sample.common.test.utils.RecyclerViewUtils.scrollTo
-import ru.surfstudio.android.sample.common.test.utils.RecyclerViewUtils.scrollToBottom
 import ru.surfstudio.android.sample.common.test.utils.ViewUtils.performClick
 import ru.surfstudio.android.sample.common.test.utils.VisibilityUtils.checkIfSnackbarIsVisible
 
@@ -26,9 +24,10 @@ class MainActivityViewTest : BaseSampleTest<MainActivityView>(MainActivityView::
     fun testMviSample() {
         performClick(R.id.main_open_input_form_btn)
         checkIfActivityIsVisible(InputFormActivityView::class.java)
-        onView(withId(R.id.input_et)).perform(typeText("message"), closeSoftKeyboard())
+        val testStr = "testStr"
+        onView(withId(R.id.input_et)).perform(typeText(testStr), closeSoftKeyboard())
         performClick(R.id.submit_btn)
-        checkIfSnackbarIsVisible(R.string.message)
+        checkIfSnackbarIsVisible(testStr)
 
         performClick(R.id.main_open_simple_list_btn)
         checkIfActivityIsVisible(SimpleListActivityView::class.java)
@@ -39,10 +38,8 @@ class MainActivityViewTest : BaseSampleTest<MainActivityView>(MainActivityView::
 
         performClick(R.id.main_open_complex_list_btn)
         checkIfActivityIsVisible(ComplexListActivityView::class.java)
-        scrollTo(R.id.reactive_rv, 20)
         onView(withId(R.id.reactive_query_tv)).perform(typeText("3"), closeSoftKeyboard())
         performClick(R.id.reactive_reload_btn)
-        scrollToBottom(R.id.reactive_rv)
         pressBack()
     }
 }
