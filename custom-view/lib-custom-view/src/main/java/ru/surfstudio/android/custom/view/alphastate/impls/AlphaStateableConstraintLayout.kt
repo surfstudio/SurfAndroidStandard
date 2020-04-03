febf9f4199c03bd3763a97c13021a7df4362c94e
Ахроров Ахбор <ahrorov@surfstudio.ru>
2020-04-03 16:26:59 +0500

    ANDDEP-947 fixed license year

--- custom-view/lib-custom-view/src/main/java/ru/surfstudio/android/custom/view/alphastate/impls/AlphaStateableConstraintLayout.kt
+++ custom-view/lib-custom-view/src/main/java/ru/surfstudio/android/custom/view/alphastate/impls/AlphaStateableConstraintLayout.kt
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2020-present, SurfStudio LLC.
+  Copyright (c) 2018-present, SurfStudio LLC.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
