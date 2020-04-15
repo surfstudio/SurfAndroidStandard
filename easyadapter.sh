b11fa91c21a2307129f0b598d1a214f85cb0974c
Margarita <margo.himera@yandex.ru>
2020-04-15 12:00:13 +0300

    ANDDEP-978 Important mirror fix: add common component as a local dependency

--- easyadapter.sh
+++ easyadapter.sh
@@ -1,4 +1,4 @@
-COMMIT_HASH=d608b6693d47f3bbc8110cdbc8954343e5311821 # 103
+COMMIT_HASH=59d7af1376db6e4f37f5bfd2a3139bd3b0730421 # fuckin last
 rm -rf _mirror temp
 git clone https://github.com/surfstudio/EasyAdapter _mirror
 git clone --single-branch --branch dev/G-0.5.0 https://gitlab.com/surfstudio/projects/standard/android-standard temp
