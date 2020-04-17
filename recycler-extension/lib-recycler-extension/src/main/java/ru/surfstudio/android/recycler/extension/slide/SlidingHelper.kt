ba0477aafaba9113eaeb1b6955e291635df22f42
Dmitry Kuzmenchuk <kuzmenchuk@surfstudio.ru>
2020-04-17 16:50:17 +0400

    ANDDEP-1022 Added more documentation, refactored outdated method names

--- recycler-extension/lib-recycler-extension/src/main/java/ru/surfstudio/android/recycler/extension/slide/SlidingHelper.kt
+++ recycler-extension/lib-recycler-extension/src/main/java/ru/surfstudio/android/recycler/extension/slide/SlidingHelper.kt
@@ -3,9 +3,10 @@ package ru.surfstudio.android.recycler.extension.slide
 import androidx.recyclerview.widget.RecyclerView
 
 /**
- * Helper used to properly setup [RecyclerView] and [BindableSlidingViewHolder].
+ * Helper used to setup [RecyclerView] to work with [BindableSlidingViewHolder]'s.
  *
- * **Note**: you should call `bind()` on creation and `unbind()` on destroy.
+ * **Note: you should call `bind()` in `onCreate()` and `unbind()` in `onDestroy()` methods
+ * to avoid memory leaks!**
  * */
 class SlidingHelper {
 
@@ -15,36 +16,44 @@ class SlidingHelper {
     private var itemClickListener: SlidingItemClickListener? = null
     private var internalRv: RecyclerView? = null
 
+    /**
+     * Setup given [RecyclerView] to work with [BindableSlidingViewHolder]'s.
+     *
+     * Adding listeners to [RecyclerView].
+     *
+     *  **Note: to avoid memory leaks - don't forget to call `unbind()` in `onDestroy()` method!**
+     * */
     fun bind(rv: RecyclerView) {
         internalRv = rv.also {
-            initItemAttachListener(it)
-            initItemTouchListener(it)
-            initItemTouchScrollListener(it)
+            initItemAttachStateListener(it)
+            initItemHorizontalScrollListener(it)
+            initItemVerticalScrollListener(it)
             initItemClickListener(it)
         }
     }
 
+    /** Remove listeners from [RecyclerView] and clear references. */
     fun unbind() {
         internalRv?.let {
-            releaseItemAttachListener(it)
-            releaseItemTouchListener(it)
-            releaseItemTouchScrollListener(it)
+            releaseItemAttachStateListener(it)
+            releaseItemHorizontalScrollListener(it)
+            releaseItemVerticalScrollListener(it)
             releaseItemClickListener(it)
         }
         internalRv = null
     }
 
-    private fun initItemAttachListener(rv: RecyclerView) {
+    private fun initItemAttachStateListener(rv: RecyclerView) {
         val watcher = SlidingItemAttachStateListener(rv).also { itemAttachStateListener = it }
         rv.addOnChildAttachStateChangeListener(watcher)
     }
 
-    private fun initItemTouchListener(rv: RecyclerView) {
+    private fun initItemHorizontalScrollListener(rv: RecyclerView) {
         val listener = SlidingItemHorizontalScrollListener(rv).also { itemHorizontalScrollListener = it }
         rv.addOnItemTouchListener(listener)
     }
 
-    private fun initItemTouchScrollListener(rv: RecyclerView) {
+    private fun initItemVerticalScrollListener(rv: RecyclerView) {
         val listener = SlidingItemVerticalScrollListener(rv).also { itemVerticalScrollListener = it }
         rv.addOnItemTouchListener(listener)
     }
@@ -54,19 +63,19 @@ class SlidingHelper {
         rv.addOnItemTouchListener(listener)
     }
 
-    private fun releaseItemAttachListener(rv: RecyclerView) {
+    private fun releaseItemAttachStateListener(rv: RecyclerView) {
         val listener = itemAttachStateListener ?: return
         rv.removeOnChildAttachStateChangeListener(listener)
         itemAttachStateListener = null
     }
 
-    private fun releaseItemTouchListener(rv: RecyclerView) {
+    private fun releaseItemHorizontalScrollListener(rv: RecyclerView) {
         val listener = itemHorizontalScrollListener ?: return
         rv.removeOnItemTouchListener(listener)
         itemHorizontalScrollListener = null
     }
 
-    private fun releaseItemTouchScrollListener(rv: RecyclerView) {
+    private fun releaseItemVerticalScrollListener(rv: RecyclerView) {
         val listener = itemVerticalScrollListener ?: return
         rv.removeOnItemTouchListener(listener)
         itemHorizontalScrollListener = null
