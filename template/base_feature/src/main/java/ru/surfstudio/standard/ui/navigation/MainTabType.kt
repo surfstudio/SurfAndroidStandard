562060e80af59b35ddd05f9fdc8d7e7a31566de1
Ruslan Sharipov <sharipov@surfstudio.ru>
2020-04-20 10:14:28 +0300

    ANDDEP-46 added default BottomBarView implementation

---
+++ template/base_feature/src/main/java/ru/surfstudio/standard/ui/navigation/MainTabType.kt
@@ -0,0 +1,11 @@
+package ru.surfstudio.standard.ui.navigation
+
+import java.io.Serializable
+
+/**
+ * Типы табов на главном экране MainActivityView
+ */
+enum class MainTabType: Serializable {
+    FEED, // экран ленты
+    PROFILE // экран профиля пользователя
+}
