d527f4f792496ddf86bf4faf929d30aa9dacff0f
Margarita <margo.himera@yandex.ru>
2020-04-20 14:45:42 +0300

    ANDDEP-1034 Add debug scripts

---
+++ core-ui.sh
@@ -0,0 +1,5 @@
+COMMIT_HASH=6bc48efdf07ca04866803ae35f0ff02ba71d1c42
+rm -rf _mirror temp
+git clone https://github.com/surfstudio/Core-ui _mirror
+git clone --single-branch --branch dev/G-0.5.0 https://gitlab.com/surfstudio/projects/standard/android-standard temp
+./gradlew deployToMirror -Pcomponent=core-ui -Pcommit=$COMMIT_HASH -PmirrorDir=_mirror -PdepthLimit=100 -PsearchLimit=100 -PmirrorUrl=https://github.com/surfstudio/Core-ui
