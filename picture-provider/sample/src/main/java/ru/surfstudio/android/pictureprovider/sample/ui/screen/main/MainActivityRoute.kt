5ceefb00fc0ea777df28d5cce01cfde650e3c9a4
Svetlana Shishkina <sshishkina@surfstudio.ru>
2020-04-23 11:25:25 +0300

    ANDDEP-805 test assemble picture provider sample

--- picture-provider/sample/src/main/java/ru/surfstudio/android/pictureprovider/sample/ui/screen/main/MainActivityRoute.kt
+++ picture-provider/sample/src/main/java/ru/surfstudio/android/pictureprovider/sample/ui/screen/main/MainActivityRoute.kt
@@ -8,7 +8,7 @@ import ru.surfstudio.android.core.ui.navigation.activity.route.ActivityRoute
  * Роутер открытия главного экрана
  */
 class MainActivityRoute : ActivityRoute() {
-    override fun prepareIntent(context: Context?): Intent {
+    override fun prepareIntent(context: Context?  ): Intent {
         return Intent(context, MainActivityView::class.java)
     }
 }
