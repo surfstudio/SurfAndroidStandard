48636211c91684c42bf9838b3f8e67527b66a0a9
Ilia Trofimenko <trofimenko@surfstudio.ru>
2020-05-03 02:18:20 +0300

    ANDDEP-1037 Removed dependency on an obsolete network module from dagger sample

---
+++ common/lib-sample-dagger/src/main/java/ru/surfstudio/android/sample/dagger/app/exceptions/NoInternetException.java
@@ -0,0 +1,11 @@
+package ru.surfstudio.android.sample.dagger.app.exceptions;
+
+/**
+ * отсутствует подключение к интернету
+ */
+public class NoInternetException extends NetworkException {
+
+    public NoInternetException(Throwable e) {
+        super(e);
+    }
+}
