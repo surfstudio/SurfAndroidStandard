71ae43e69e86b3ce2fcdf1bb1fffdc816856f69f
ryabkov-surf <ryabkov@surfstudio.ru>
2020-05-27 17:28:29 +0300

    ANDDEP-704 revert commit

--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/exceptions/deploy_to_mirror/MirrorCommitNotFoundByStandardHashException.kt
+++
@@ -1,7 +0,0 @@
-package ru.surfstudio.android.build.exceptions.deploy_to_mirror
-
-import org.gradle.api.GradleException
-
-class MirrorCommitNotFoundByStandardHashException(hash: String) : GradleException(
-        "Commit not found in mirror with hash = $hash"
-)
--- buildSrc/src/main/kotlin/ru/surfstudio/android/build/exceptions/deploy_to_mirror/RevCommitNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package ru.surfstudio.android.build.exceptions.deploy_to_mirror
-
-import org.gradle.api.GradleException
-
-class RevCommitNotFoundException(commitHash: String) : GradleException(
-        "RevCommit with hash \"$commitHash\" not found"
-)
