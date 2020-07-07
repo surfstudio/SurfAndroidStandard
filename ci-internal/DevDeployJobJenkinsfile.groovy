34217024635756f2d729eddd1ff194ee2d19af15
ryabkov-surf <ryabkov@surfstudio.ru>
2020-07-07 18:19:51 +0300

    ANDDER-734 тест

--- ci-internal/DevDeployJobJenkinsfile.groovy
+++ ci-internal/DevDeployJobJenkinsfile.groovy
@@ -101,7 +101,7 @@ pipeline.stages = [
             )
             script.sh "git checkout -B $branchName origin/$branchName"
 
-            // тест
+            // тест 1
             RepositoryUtil.saveCurrentGitCommitHash(script)
         },
         pipeline.stage(NOTIFY_ABOUT_NEW_RELEASE_NOTES, StageStrategy.SKIP_STAGE, false) {
