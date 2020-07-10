9347280b1e52609df93c08d71e6051fd6fcf6455
ryabkov-surf <ryabkov@surfstudio.ru>
2020-07-10 18:22:40 +0300

    ANDDER-734 тест

--- ci-internal/DevDeployJobJenkinsfile.groovy
+++ ci-internal/DevDeployJobJenkinsfile.groovy
@@ -95,7 +95,7 @@ pipeline.initializeBody = {
 
 pipeline.stages = [
         pipeline.stage(CHECKOUT) {
-            // тест 1
+            // тест
             script.git(
                     url: pipeline.repoUrl,
                     credentialsId: pipeline.repoCredentialsId
