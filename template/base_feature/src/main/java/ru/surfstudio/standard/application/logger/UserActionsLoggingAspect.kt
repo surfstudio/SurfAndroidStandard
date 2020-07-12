58a3a2843088cef40cbf7f89543f8b4e23b3cd31
Ilia Trofimenko <trofimenko@surfstudio.ru>
2020-07-12 18:26:47 +0300

    ANDDEP-1080 implemented logging for rxbinding click handler

--- template/base_feature/src/main/java/ru/surfstudio/standard/application/logger/UserActionsLoggingAspect.kt
+++ template/base_feature/src/main/java/ru/surfstudio/standard/application/logger/UserActionsLoggingAspect.kt
@@ -3,9 +3,11 @@
 package ru.surfstudio.standard.application.logger
 
 import android.view.View
+import io.reactivex.Observable
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
-import org.aspectj.lang.annotation.Pointcut
 import timber.log.Timber
 
 /**
@@ -15,18 +17,25 @@ import timber.log.Timber
 class UserActionsLoggingAspect {
 
     /**
-     * View click method template
+     *  Default click handler
      */
-    @Pointcut("execution(void *.onClick(..))")
-    fun onViewClick() {
-        //do nothing
+    @Before("execution(void *.onClick(..)) && args(view)")
+    fun onClickAdvice(view: View) {
+        logClick(view)
     }
 
     /**
-     * View click handler
+     * RxBinding click handler
      */
-    @Before("onViewClick() && args(view)")
-    fun onClickAdvice(view: View) {
+    @Around("call(* *.clicks(..))")
+    fun onClickAdvice(joinPoint: ProceedingJoinPoint): Any {
+        val view = joinPoint.args[0] as View
+        return (joinPoint.proceed() as Observable<*>).doOnNext {
+            logClick(view)
+        }
+    }
+
+    private fun logClick(view: View) {
         val location = IntArray(2).apply {
             view.getLocationOnScreen(this)
         }
