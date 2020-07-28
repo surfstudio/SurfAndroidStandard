acf203a363dd7ea40195d91eb7e9ef825443daf3
ryabkov-surf <ryabkov@surfstudio.ru>
2020-07-28 19:12:34 +0300

    ANDDEP-1080 отмена коммита

--- template/base_feature/src/main/java/ru/surfstudio/standard/application/logger/user_actions_logger/UserActionsLoggingAspect.kt
+++ template/base_feature/src/main/java/ru/surfstudio/standard/application/logger/user_actions_logger/UserActionsLoggingAspect.kt
@@ -2,20 +2,13 @@
 
 package ru.surfstudio.standard.application.logger.user_actions_logger
 
-import android.view.MotionEvent
 import android.view.View
-import android.widget.CheckBox
-import android.widget.EditText
 import io.reactivex.Observable
-import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
-import org.aspectj.lang.reflect.MethodSignature
 import timber.log.Timber
-import java.lang.reflect.Method
 
 /**
  * The aspect that implements the logic of logging user actions
@@ -29,8 +22,19 @@ class UserActionsLoggingAspect {
     @Before("execution(void *.onClick(..)) && args(view)")
     fun onClickAdvice(view: View) = log(ViewClick(view).toString())
 
+    /**
+     * RxBinding click handler
+     */
+    @Around("call(io.reactivex.Observable com.jakewharton.rxbinding2.view.RxView.clicks(..)) && args(view)")
+    fun onClickAdvice(joinPoint: ProceedingJoinPoint, view: View): Any {
+        return (joinPoint.proceed() as Observable<*>).doOnNext {
+            log(ViewClick(view).toString())
+        }
+    }
+
     private fun log(message: String) {
-        Timber.tag(TAG).d(message)
+        Timber.tag(TAG)
+        Timber.d(message)
     }
 
     companion object {
