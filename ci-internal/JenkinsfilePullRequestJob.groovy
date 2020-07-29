7de6de23831451244ad7578278d377b48d7374eb
repin <mr.malarg@gmail.com>
2020-07-29 21:20:58 +0500

    chmod returned

--- ci-internal/JenkinsfilePullRequestJob.groovy
+++ ci-internal/JenkinsfilePullRequestJob.groovy
@@ -226,6 +226,7 @@ pipeline.stages = [
             script.sh("./gradlew checkReleaseNotesChanged -PrevisionToCompare=${lastDestinationBranchCommitHash}")
         },
         pipeline.stage(CHECK_LICENSE_ADDED, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+            script.sh "chmod 755 ci-internal/auto-add-license/add_license.sh"
             script.sh "./ci-internal/auto-add-license/add_license.sh"
         },
         pipeline.stage(CHECKS_RESULT) {
