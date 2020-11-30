60dee7c4387c6253df887e9986bbde5e33d0d121
Vladislav Isayev <isaev@surfstudio.ru>
2020-11-30 13:46:54 +0300

    ANDDEP-1136 Таб поиск переписан на MVI

--- template/f-search/src/main/java/ru/surfstudio/standard/f_search/di/SearchScreenConfigurator.kt
+++ template/f-search/src/main/java/ru/surfstudio/standard/f_search/di/SearchScreenConfigurator.kt
@@ -4,31 +4,52 @@ import android.os.Bundle
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import ru.surfstudio.android.core.mvi.impls.event.hub.ScreenEventHub
+import ru.surfstudio.android.core.mvi.impls.event.hub.dependency.ScreenEventHubDependency
+import ru.surfstudio.android.core.mvi.impls.ui.binder.ScreenBinder
+import ru.surfstudio.android.core.mvi.impls.ui.binder.ScreenBinderDependency
 import ru.surfstudio.android.core.mvp.configurator.BindableScreenComponent
 import ru.surfstudio.android.core.mvp.configurator.ScreenComponent
 import ru.surfstudio.android.dagger.scope.PerScreen
-import ru.surfstudio.standard.f_search.SearchFragmentView
-import ru.surfstudio.standard.f_search.SearchPresenter
+import ru.surfstudio.standard.f_search.*
 import ru.surfstudio.standard.ui.activity.di.ActivityComponent
 import ru.surfstudio.standard.ui.activity.di.FragmentScreenConfigurator
 import ru.surfstudio.standard.ui.navigation.routes.SearchFragmentRoute
 import ru.surfstudio.standard.ui.screen_modules.CustomScreenModule
 import ru.surfstudio.standard.ui.screen_modules.FragmentScreenModule
 
-class SearchScreenConfigurator : FragmentScreenConfigurator(Bundle.EMPTY) {
+/**
+ * Конфигуратор таба поиск [SearchFragmentView]
+ */
+internal class SearchScreenConfigurator(arguments: Bundle?) : FragmentScreenConfigurator(arguments) {
 
     @PerScreen
-    @Component(dependencies = [ActivityComponent::class],
-            modules = [FragmentScreenModule::class, SearchScreenModule::class])
-    interface SearchScreenComponent : BindableScreenComponent<SearchFragmentView>
+    @Component(
+            dependencies = [ActivityComponent::class],
+            modules = [FragmentScreenModule::class, SearchScreenModule::class]
+    )
+    internal interface SearchScreenComponent : BindableScreenComponent<SearchFragmentView>
 
     @Module
-    class SearchScreenModule(route: SearchFragmentRoute) :
-            CustomScreenModule<SearchFragmentRoute>(route) {
+    internal class SearchScreenModule(route: SearchFragmentRoute) : CustomScreenModule<SearchFragmentRoute>(route) {
 
         @Provides
         @PerScreen
-        fun provideSearchPresenter(presenter: SearchPresenter) = Any()
+        fun provideEventHub(
+                screenEventHubDependency: ScreenEventHubDependency
+        ) = ScreenEventHub<SearchEvent>(screenEventHubDependency, SearchEvent::Lifecycle)
+
+        @Provides
+        @PerScreen
+        fun provideBinder(
+                screenBinderDependency: ScreenBinderDependency,
+                eventHub: ScreenEventHub<SearchEvent>,
+                mw: SearchMiddleware,
+                sh: SearchScreenStateHolder,
+                reducer: SearchReducer
+        ): Any = ScreenBinder(screenBinderDependency).apply {
+            bind(eventHub, mw, sh, reducer)
+        }
     }
 
     override fun createScreenComponent(
