ffbc67d32d4807f88f8ae8f84565bf8722775746
Vladislav Isayev <isaev@surfstudio.ru>
2020-11-30 14:08:44 +0300

    ANDDEP-1136 Таб новости переписан на MVI

--- template/f-feed/src/main/java/ru/surfstudio/standard/f_feed/di/FeedScreenConfigurator.kt
+++ template/f-feed/src/main/java/ru/surfstudio/standard/f_feed/di/FeedScreenConfigurator.kt
@@ -4,30 +4,52 @@ import android.os.Bundle
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import ru.surfstudio.android.core.mvi.impls.event.hub.ScreenEventHub
+import ru.surfstudio.android.core.mvi.impls.event.hub.dependency.ScreenEventHubDependency
+import ru.surfstudio.android.core.mvi.impls.ui.binder.ScreenBinder
+import ru.surfstudio.android.core.mvi.impls.ui.binder.ScreenBinderDependency
 import ru.surfstudio.android.core.mvp.configurator.BindableScreenComponent
 import ru.surfstudio.android.core.mvp.configurator.ScreenComponent
 import ru.surfstudio.android.dagger.scope.PerScreen
-import ru.surfstudio.standard.f_feed.FeedFragmentView
-import ru.surfstudio.standard.f_feed.FeedPresenter
+import ru.surfstudio.standard.f_feed.*
 import ru.surfstudio.standard.ui.activity.di.ActivityComponent
 import ru.surfstudio.standard.ui.activity.di.FragmentScreenConfigurator
 import ru.surfstudio.standard.ui.navigation.routes.FeedFragmentRoute
 import ru.surfstudio.standard.ui.screen_modules.CustomScreenModule
 import ru.surfstudio.standard.ui.screen_modules.FragmentScreenModule
 
-class FeedScreenConfigurator : FragmentScreenConfigurator(Bundle.EMPTY) {
+/**
+ * Конфигуратор таба новости [FeedFragmentView]
+ */
+internal class FeedScreenConfigurator(arguments: Bundle?) : FragmentScreenConfigurator(arguments) {
 
     @PerScreen
-    @Component(dependencies = [ActivityComponent::class],
-            modules = [FragmentScreenModule::class, FeedScreenModule::class])
-    interface FeedScreenComponent : BindableScreenComponent<FeedFragmentView>
+    @Component(
+            dependencies = [ActivityComponent::class],
+            modules = [FragmentScreenModule::class, FeedScreenModule::class]
+    )
+    internal interface FeedScreenComponent : BindableScreenComponent<FeedFragmentView>
 
     @Module
-    class FeedScreenModule(route: FeedFragmentRoute) : CustomScreenModule<FeedFragmentRoute>(route) {
+    internal class FeedScreenModule(route: FeedFragmentRoute) : CustomScreenModule<FeedFragmentRoute>(route) {
 
-        @PerScreen
         @Provides
-        fun provideFeedPresenter(presenter: FeedPresenter) = Any()
+        @PerScreen
+        fun provideEventHub(
+                screenEventHubDependency: ScreenEventHubDependency
+        ) = ScreenEventHub<FeedEvent>(screenEventHubDependency, FeedEvent::Lifecycle)
+
+        @Provides
+        @PerScreen
+        fun provideBinder(
+                screenBinderDependency: ScreenBinderDependency,
+                eventHub: ScreenEventHub<FeedEvent>,
+                mw: FeedMiddleware,
+                sh: FeedScreenStateHolder,
+                reducer: FeedReducer
+        ): Any = ScreenBinder(screenBinderDependency).apply {
+            bind(eventHub, mw, sh, reducer)
+        }
     }
 
     override fun createScreenComponent(
