80b80f9a6af23c485afe036f3e1d417250589a24
Vladislav Isayev <isaev@surfstudio.ru>
2020-12-08 13:18:20 +0300

    ANDDEP-1136 Правки в MR

--- template/base_feature/src/main/java/ru/surfstudio/standard/ui/mvi/di/NavigationMiddlewareModule.kt
+++ template/base_feature/src/main/java/ru/surfstudio/standard/ui/mvi/di/NavigationMiddlewareModule.kt
@@ -3,7 +3,6 @@ package ru.surfstudio.standard.ui.mvi.di
 import dagger.Module
 import dagger.Provides
 import ru.surfstudio.android.core.mvi.impls.ui.freezer.LifecycleSubscriptionFreezer
-import ru.surfstudio.android.core.ui.event.ScreenEventDelegateManager
 import ru.surfstudio.android.dagger.scope.PerScreen
 import ru.surfstudio.android.navigation.executor.NavigationCommandExecutor
 import ru.surfstudio.standard.ui.mvi.navigation.AppNavigationMiddleware
@@ -13,14 +12,6 @@ import ru.surfstudio.standard.ui.mvi.navigation.AppNavigationMiddleware
  */
 @Module
 class NavigationMiddlewareModule {
-
-    @PerScreen
-    @Provides
-    internal fun provideLifecycleSubscriptionFreezer(
-            eventDelegateManager: ScreenEventDelegateManager
-    ): LifecycleSubscriptionFreezer {
-        return LifecycleSubscriptionFreezer(eventDelegateManager)
-    }
 
     @Provides
     @PerScreen
