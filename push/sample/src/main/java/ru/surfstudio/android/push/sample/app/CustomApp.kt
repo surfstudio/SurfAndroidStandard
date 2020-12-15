2d99bd547b8f417c605e41571adc56b40b1eef3f
Ахбор Ахроров <ahrorov@surfstudio.ru>
2020-12-15 16:49:09 +0500

    ANDDEP-930 logs on notification actions

--- push/sample/src/main/java/ru/surfstudio/android/push/sample/app/CustomApp.kt
+++ push/sample/src/main/java/ru/surfstudio/android/push/sample/app/CustomApp.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.multidex.MultiDexApplication
 import ru.surfstudio.android.activity.holder.ActiveActivityHolder
+import ru.surfstudio.android.logger.Logger
 import ru.surfstudio.android.notification.ui.PushClickProvider
 import ru.surfstudio.android.notification.ui.PushEventListener
 import ru.surfstudio.android.sample.dagger.app.DefaultActivityLifecycleCallbacks
@@ -54,15 +55,15 @@ class CustomApp : MultiDexApplication() {
     private fun initPushEventListener() {
         PushClickProvider.pushEventListener = object : PushEventListener {
             override fun pushDismissListener(context: Context, intent: Intent) {
-                //todo
+                Logger.i("Push notification dismissed")
             }
 
             override fun pushOpenListener(context: Context, intent: Intent) {
-                //todo
+                Logger.i("Push notification opened")
             }
 
             override fun customActionListener(context: Context, intent: Intent) {
-                //todo
+                Logger.i("Push notification custom action")
             }
         }
     }
