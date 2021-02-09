46d2d35f48e3c10c0b3408a0ac8c55ed81c166cc
Маргарита Володина <volodina@surfstudio.ru>
2021-02-09 13:39:15 +0000

    Update CoreActivity.java

--- core-ui/lib-core-ui/src/main/java/ru/surfstudio/android/core/ui/activity/CoreActivity.java
+++ core-ui/lib-core-ui/src/main/java/ru/surfstudio/android/core/ui/activity/CoreActivity.java
@@ -93,7 +93,7 @@ public abstract class CoreActivity extends AppCompatActivity implements CoreActi
      *
      * @param savedInstanceState
      */
-    void onPreCreate(Bundle savedInstanceState) {
+    protected void onPreCreate(Bundle savedInstanceState) {
         //empty
     }
 
