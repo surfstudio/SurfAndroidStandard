cafe39b91e987874fcfbb2d9e765a2c716bc5369
valerijsetrakov <valerasetrakov@gmail.com>
2021-03-24 17:29:19 +0300

    1) Changed base scheduler for presenter subscribe methods from schedulersProvider.main() to MainThreadImmediateScheduler

--- mvp/lib-core-mvp/src/main/java/ru/surfstudio/android/core/mvp/presenter/BasePresenter.java
+++ mvp/lib-core-mvp/src/main/java/ru/surfstudio/android/core/mvp/presenter/BasePresenter.java
@@ -38,6 +38,7 @@ import ru.surfstudio.android.core.mvp.view.CoreView;
 import ru.surfstudio.android.core.ui.navigation.activity.navigator.ActivityNavigator;
 import ru.surfstudio.android.rx.extension.ActionSafe;
 import ru.surfstudio.android.rx.extension.ConsumerSafe;
+import ru.surfstudio.android.rx.extension.scheduler.MainThreadImmediateScheduler;
 import ru.surfstudio.android.rx.extension.scheduler.SchedulersProvider;
 
 /**
@@ -80,28 +81,28 @@ public abstract class BasePresenter<V extends CoreView> extends CorePresenter<V>
     protected <T> Disposable subscribe(final Observable<T> observable,
                                        final ObservableOperatorFreeze<T> operator,
                                        final LambdaObserver<T> observer) {
-        return super.subscribe(observable.observeOn(schedulersProvider.main(), true), operator, observer);
+        return super.subscribe(observable.observeOn(MainThreadImmediateScheduler.INSTANCE, true), operator, observer);
     }
 
     @Override
     protected <T> Disposable subscribe(Single<T> single,
                                        SingleOperatorFreeze<T> operator,
                                        DisposableSingleObserver<T> observer) {
-        return super.subscribe(single.observeOn(schedulersProvider.main()), operator, observer);
+        return super.subscribe(single.observeOn(MainThreadImmediateScheduler.INSTANCE), operator, observer);
     }
 
     @Override
     protected Disposable subscribe(Completable completable,
                                    CompletableOperatorFreeze operator,
                                    DisposableCompletableObserver observer) {
-        return super.subscribe(completable.observeOn(schedulersProvider.main()), operator, observer);
+        return super.subscribe(completable.observeOn(MainThreadImmediateScheduler.INSTANCE), operator, observer);
     }
 
     @Override
     protected <T> Disposable subscribe(Maybe<T> maybe,
                                        MaybeOperatorFreeze<T> operator,
                                        DisposableMaybeObserver<T> observer) {
-        return super.subscribe(maybe.observeOn(schedulersProvider.main()), operator, observer);
+        return super.subscribe(maybe.observeOn(MainThreadImmediateScheduler.INSTANCE), operator, observer);
     }
     //endregion
 
