135eb86c2f158acc1a96f4a19983fa6f8734c870
volodina-surf <volodina@surfstudio.ru>
2021-04-08 16:55:25 +0300

    remove spoon check

--- ci-internal/JenkinsfilePullRequestJob.groovy
+++ ci-internal/JenkinsfilePullRequestJob.groovy
@@ -1,4 +1,4 @@
-@Library('surf-lib@version-4.0.0-SNAPSHOT')
+@Library('surf-lib@remove-spoon')
 // https://github.com/surfstudio/jenkins-pipeline-lib
 
 import ru.surfstudio.ci.*
@@ -189,14 +189,14 @@ pipeline.stages = [
             script.sh "git merge origin/$destinationBranch --no-ff"
         },
 
-        pipeline.stage(RELEASE_NOTES_DIFF, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(RELEASE_NOTES_DIFF, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew generateReleaseNotesDiff -PrevisionToCompare=${lastDestinationBranchCommitHash}")
         },
 
-        pipeline.stage(CHECK_CONFIGURATION_IS_NOT_PROJECT_SNAPSHOT) {
+        pipeline.stage(CHECK_CONFIGURATION_IS_NOT_PROJECT_SNAPSHOT, StageStrategy.SKIP_STAGE) {
             script.sh "./gradlew checkConfigurationIsNotProjectSnapshotTask"
         },
-        pipeline.stage(CHECK_STABLE_MODULES_IN_ARTIFACTORY, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(CHECK_STABLE_MODULES_IN_ARTIFACTORY, StageStrategy.SKIP_STAGE) {
             withArtifactoryCredentials(script) {
                 script.echo "artifactory user: ${script.env.surf_maven_username}"
                 script.sh("./gradlew checkStableArtifactsExistInArtifactoryTask")
@@ -208,27 +208,27 @@ pipeline.stages = [
             }
 
         },
-        pipeline.stage(CHECK_STABLE_MODULES_NOT_CHANGED, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(CHECK_STABLE_MODULES_NOT_CHANGED, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew checkStableComponentsChanged -PrevisionToCompare=${lastDestinationBranchCommitHash}")
         },
         pipeline.stage(CHECK_UNSTABLE_MODULES_DO_NOT_BECAME_STABLE, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew checkUnstableToStableChanged -PrevisionToCompare=${lastDestinationBranchCommitHash}")
         },
-        pipeline.stage(CHECK_MODULES_IN_DEPENDENCY_TREE_OF_STABLE_MODULE_ALSO_STABLE, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(CHECK_MODULES_IN_DEPENDENCY_TREE_OF_STABLE_MODULE_ALSO_STABLE, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew checkStableComponentStandardDependenciesStableTask")
         },
-        pipeline.stage(CHECK_RELEASE_NOTES_VALID, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(CHECK_RELEASE_NOTES_VALID, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew checkReleaseNotesContainCurrentVersion")
             script.sh("./gradlew checkReleaseNotesNotContainCyrillic")
         },
-        pipeline.stage(CHECK_RELEASE_NOTES_CHANGED, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(CHECK_RELEASE_NOTES_CHANGED, StageStrategy.SKIP_STAGE) {
             script.sh("./gradlew checkReleaseNotesChanged -PrevisionToCompare=${lastDestinationBranchCommitHash}")
         },
-        pipeline.stage(ADD_LICENSE, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(ADD_LICENSE, StageStrategy.SKIP_STAGE) {
             script.sh "chmod 755 ci-internal/auto-add-license/add_license.sh"
             script.sh "./ci-internal/auto-add-license/add_license.sh"
         },
-        pipeline.stage(CHECKS_RESULT) {
+        pipeline.stage(CHECKS_RESULT, StageStrategy.SKIP_STAGE) {
             script.sh "rm -rf $TEMP_FOLDER_NAME"
             def checksPassed = true
             [
@@ -254,9 +254,9 @@ pipeline.stages = [
         },
 
         pipeline.stage(BUILD) {
-            AndroidPipelineHelper.buildStageBodyAndroid(script, "clean assemble")
+            AndroidPipelineHelper.buildStageBodyAndroid(script, "clean :easyadapter-sample:assembleDebug")
         },
-        pipeline.stage(BUILD_TEMPLATE, StageStrategy.UNSTABLE_WHEN_STAGE_ERROR) {
+        pipeline.stage(BUILD_TEMPLATE, StageStrategy.SKIP_STAGE) {
             script.sh("echo \"androidStandardDebugDir=$workspace\n" +
                     "androidStandardDebugMode=true\n" +
                     "skipSamplesBuild=true\" > template/android-standard/androidStandard.properties")
@@ -266,7 +266,7 @@ pipeline.stages = [
              */
             script.sh("./gradlew -p template clean build assembleQa assembleDebug --stacktrace")
         },
-        pipeline.stage(UNIT_TEST) {
+        pipeline.stage(UNIT_TEST, StageStrategy.SKIP_STAGE) {
             AndroidPipelineHelper.unitTestStageBodyAndroid(script,
                     "testReleaseUnitTest",
                     "**/test-results/testReleaseUnitTest/*.xml",
@@ -291,14 +291,14 @@ pipeline.stages = [
                 )
             }
         },
-        pipeline.stage(INSTRUMENTATION_TEST, StageStrategy.SKIP_STAGE) {
+        pipeline.stage(INSTRUMENTATION_TEST) {
             AndroidPipelineHelper.instrumentationTestStageBodyAndroid(
                     script,
                     new AvdConfig(),
                     "debug",
                     getTestInstrumentationRunnerName,
                     new AndroidTestConfig(
-                            "assembleAndroidTest",
+                            ":easyadapter-sample:assembleDebugAndroidTest",
                             "build/outputs/androidTest-results/instrumental",
                             "build/reports/androidTests/instrumental",
                             true,
