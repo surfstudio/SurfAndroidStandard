5ce669e66cc45f576e8ae89c7e95915ae2b2f5e4
Margarita <margo.himera@yandex.ru>
2021-11-03 18:38:48 +0300

    Fix BasePushHandleStrategy

--- template/base_feature/src/main/java/ru/surfstudio/standard/application/notification/strategy/BasePushHandleStrategy.kt
+++ template/base_feature/src/main/java/ru/surfstudio/standard/application/notification/strategy/BasePushHandleStrategy.kt
@@ -1,17 +1,26 @@
 package ru.surfstudio.standard.application.notification.strategy
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import ru.surfstudio.android.notification.interactor.push.PushInteractor
 import ru.surfstudio.android.notification.ui.notification.groups.NotificationsGroup
 import ru.surfstudio.android.notification.ui.notification.strategies.SimpleAbstractPushHandleStrategy
 import ru.surfstudio.android.template.base_feature.R
+import ru.surfstudio.android.utilktx.util.SdkUtils
 import ru.surfstudio.standard.application.notification.type.NotificationTypeData
 import ru.surfstudio.standard.ui.navigation.routes.MainActivityRoute
+import kotlin.math.absoluteValue
 
 /**
  * Базовая стратегия обработки push-уведомления
- * todo исправит исходя из нужд приложения
+ * todo исправить исходя из нужд приложения
  */
 class BasePushHandleStrategy : SimpleAbstractPushHandleStrategy<NotificationTypeData>() {
 
@@ -29,12 +38,55 @@ class BasePushHandleStrategy : SimpleAbstractPushHandleStrategy<NotificationType
     override val color: Int
         get() = R.color.colorAccent
 
-    //todo исправить из нужд приложения
     override val group = NotificationsGroup("Messages")
 
-    //todo исправить из нужд приложения
-    override fun coldStartIntent(context: Context): Intent? =
+    override fun coldStartIntent(context: Context): Intent =
             MainActivityRoute().createIntent(context)
 
     override fun handlePushInActivity(activity: Activity) = false
+
+    override fun handle(
+        context: Context,
+        pushInteractor: PushInteractor,
+        uniqueId: Int,
+        title: String,
+        body: String
+    ) {
+        super.handle(
+            context,
+            pushInteractor,
+            uniqueId.absoluteValue,
+            title,
+            body
+        )
+    }
+
+    override fun makeNotificationBuilder(
+        context: Context,
+        title: String,
+        body: String
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, context.getString(channelId))
+            .setDefaults(0)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(icon)
+            .setColor(ContextCompat.getColor(context, color))
+            .setAutoCancel(autoCancelable)
+            .setGroup(group.groupAlias)
+            .setContentIntent(pendingIntent)
+            .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun makeNotificationChannel(context: Context, title: String): NotificationChannel? {
+        return if (SdkUtils.isAtLeastOreo()) {
+            val id = context.resources.getString(channelId)
+            val name = context.resources.getString(channelName)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            NotificationChannel(id, name, importance)
+        } else {
+            null
+        }
+    }
 }
